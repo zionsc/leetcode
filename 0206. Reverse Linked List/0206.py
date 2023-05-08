b23eb2d8d6c7c8df6a35cf7d64f82339bc46687d
Seeon Chang <zionchan@usc.edu>
2023-05-08 15:27:42 -0700

    0206-

--- 0206. Reverse Linked List/0206.py
+++ 0206. Reverse Linked List/0206.py
@@ -5,11 +5,27 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        if not head: # if head is NULL
+            return None
 
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        return prev
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            
+        head.next = None
+        return newHead
+
+
+
+
+
+
+        # prev, curr = None, head
+
+        # while curr:
+        #     next = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = next
+        # return prev 
