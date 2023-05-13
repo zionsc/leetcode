27548d33a61e37be72196ae93bcb63e9c0523def
Seeon Chang <zionchan@usc.edu>
2023-05-12 22:38:47 -0700

    0238

---
+++ 0238. Product of Array Except Self/0238.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums)) # every index is 1, there are len(nums) indexes
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1): #from end to -1, decrement
+            res[i] = postfix
+            postfix *= nums[i]
+        return res
+
