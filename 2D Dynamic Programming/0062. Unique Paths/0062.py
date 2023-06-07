f09b90299ac98dc166fe8e9a7df4eca58a7e0a79
Seeon Chang <zionchan@usc.edu>
2023-06-06 21:35:58 -0700

    ordered

---
+++ 2D Dynamic Programming/0062. Unique Paths/0062.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        arr = [[0]*(n+1) for i in range(m+1)] # must use list comprehension, or everything is by reference
+        arr[m-1][n-1] = 1
+
+        print(arr)
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                arr[i][j] = arr[i][j+1] + arr[i+1][j]
+                arr[m-1][n-1] = 1
+        
+        
+        return arr[0][0]
