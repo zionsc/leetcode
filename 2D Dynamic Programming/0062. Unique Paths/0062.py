0215bf847189a146d910a4bae6405aa34973ca60
Seeon Chang <zionchan@usc.edu>
2023-06-06 21:45:46 -0700

    0062

--- 2D Dynamic Programming/0062. Unique Paths/0062.py
+++ 2D Dynamic Programming/0062. Unique Paths/0062.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
                 arr[i][j] = arr[i][j+1] + arr[i+1][j]
-                arr[m-1][n-1] = 1
-        
+                arr[m-1][n-1] = 1 # since it updates arr[m-1][n-1] to 0 in the first iteration, but we need it to be 1 HARD CODE LOL        
         
         return arr[0][0]
