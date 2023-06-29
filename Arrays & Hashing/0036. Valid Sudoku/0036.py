568a50ce95e9d783c53104c07dcbb01e012685f0
Seeon Chang <zionchan@usc.edu>
2023-06-29 12:54:02 -0700

    0036

--- Arrays & Hashing/0036. Valid Sudoku/0036.py
+++ Arrays & Hashing/0036. Valid Sudoku/0036.py
@@ -1,25 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = defaultdict(set) #adds to set if not found instead of key error
-        rows = defaultdict(set) #hashmap for both
-        squares = defaultdict(set) #key = (r/3, c/3) 
+        rows = defaultdict(set) #
+        cols = defaultdict(set) # --> need to store values from each set --> thus must have set in order to check each row/col
+        squares = defaultdict(set) # --> key = r//3, c//3 becuase it will then always be 0 1 2 x 0 1 2, checking each of the 9 boards.
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
+                
+                elif board[r][c] != ".":
+                    if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[r//3, c//3]: 
+                        return False
 
-                # if inside already, return false
-                if (board[r][c] in rows[r] or 
-                    board[r][c] in cols[c] or 
-                    board[r][c] in squares[(r//3, c//3)]):
-                    return False
-
-                # if it has reached here (if not inside--
-                #the hashsets already, then add and iterate again)
-                # since the objects are hashsets, key is whatever and the value is a set (multiple values)
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    squares[r//3, c//3].add(board[r][c]) # must do // for integer div --> / returns float point number
         
         return True
