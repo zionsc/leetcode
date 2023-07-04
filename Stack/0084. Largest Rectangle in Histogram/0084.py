63ca571bdb370a5720f1aab4cd00da492b148b13
Seeon Chang <zionchan@usc.edu>
2023-07-04 15:25:15 -0700

    0084

--- Stack/0084. Largest Rectangle in Histogram/0084.py
+++ Stack/0084. Largest Rectangle in Histogram/0084.py
@@ -9,7 +9,8 @@ class Solution:
             # Basically the idea of the while loop is to pop all values of the stack that are greater in height than the discovered bar. -->
             # before popping though, the maxArea for them would be compared to the current maxArea (maxArea for them could extend backwards, thats why we update start accordingly)
             # do this until the bar's height no longer is less than the stack[-1], meaning that the rectangle created by this bar starts at that given index. 
-            # Always make the index of the bar in the stack be the start value fo the rectangle given that the rectangle extends backwards as far as it can 
+            # Always make the index of the bar in the stack be the start value fo the rectangle given that the rectangle extends backwards as far as it can
+            # values that are simply just added as they are the tallest bar would have their start index just be where they were discovered as they cannot extend backwards. 
 
             while stack and stack[-1][1] > h: # Stack is not empty, the found height is less than the stack height
                 index, height = stack.pop() # pop from stack, retrieve the data from the popped bar
