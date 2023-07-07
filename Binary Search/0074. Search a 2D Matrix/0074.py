819d7f77a01a77a6b0a3e9b6d0ee97290450eab8
Seeon Chang <zionchan@usc.edu>
2023-07-07 13:34:25 -0700

    0074

---
+++ Binary Search/0074. Search a 2D Matrix/0074.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        # find the row its in
+        while top <= bot:
+            # mid row
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        
+        if not (top <= bot): # not found in any row
+           return False 
+
+        # find the value in the row
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+
+        return False
