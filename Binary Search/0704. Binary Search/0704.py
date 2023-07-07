8d0f4e70bc9f64dd58a4bfc66101ace1eb0efa5f
Seeon Chang <zionchan@usc.edu>
2023-07-07 12:39:32 -0700

    0704

--- Binary Search/0704. Binary Search/0704.py
+++ Binary Search/0704. Binary Search/0704.py
@@ -4,5 +4,10 @@ class Solution:
         return self.binarysearch(nums, target, l, r)
 
     def binarysearch(self, nums, target, l, right):
+        # make sure it does not go out of bounds
         if right >= left:
             mid = l + ((r - l) // 2)
+
+            # base case
+            if nums[mid] == target:
+                return mid
