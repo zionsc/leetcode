9317f4ccbd81602df36de1b29d08f6efecae7097
Seeon Chang <zionchan@usc.edu>
2023-07-08 19:09:48 -0700

    0875

--- Binary Search/0875. Koko Eating Bananas/0875.py
+++ Binary Search/0875. Koko Eating Bananas/0875.py
@@ -1,4 +1,7 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Basic Idea: Go through from 1 to max number in piles to see which value is the highest possible k we can have.
+        # Max piles should be the biggest k as anything bigger does not really make a difference in our time. 
         l, r = 1, max(piles)
+
         
