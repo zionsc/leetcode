c24a5daf7ad021cd38e2e77fd68435dddbbf193d
Seeon Chang <zionchan@usc.edu>
2023-07-08 19:10:20 -0700

    0875

--- Binary Search/0875. Koko Eating Bananas/0875.py
+++ Binary Search/0875. Koko Eating Bananas/0875.py
@@ -3,5 +3,9 @@ class Solution:
         # Basic Idea: Go through from 1 to max number in piles to see which value is the highest possible k we can have.
         # Max piles should be the biggest k as anything bigger does not really make a difference in our time. 
         l, r = 1, max(piles)
+        res = max(piles)
+
+        while l <= r:
+            
 
         
