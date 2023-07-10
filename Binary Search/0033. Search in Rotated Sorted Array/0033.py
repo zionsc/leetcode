b45b38b67df068dc3dbf46e3cc1316d7ae2bad36
Seeon Chang <zionchan@usc.edu>
2023-07-10 09:23:15 -0700

    0033

--- Binary Search/0033. Search in Rotated Sorted Array/0033.py
+++ Binary Search/0033. Search in Rotated Sorted Array/0033.py
@@ -9,4 +9,19 @@ class Solution:
                 return mid
 
             # left-side is sorted (strictly increasing)
-            if nums[l] <= nums[mid]
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]: # must be in right-side -> greater than mid or less than l
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            # right-side is sorted (strictly increasing)
+            else:
+                if target < nums[mid] or target > nums[r]: # must be in left-side -> less than mid or greater than r
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            
+            return -1 
+
+
