04bb0a4bfb27b8b6b3956549ac0075c47911e8da
Seeon Chang <zionchan@usc.edu>
2023-07-11 00:58:20 -0700

    0981

--- Binary Search/0981. Time Based Key-Value Store/0981.py
+++ Binary Search/0981. Time Based Key-Value Store/0981.py
@@ -11,4 +11,10 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         res = "" # res is initialized to an empty list in order to make sure that it is simply returned if not found in keys
+        values = self.storage.get(key, []) # values = storage[key]'s list of pairs. Else it returns an empty list []
+
+        m = l + ((r - l) // 2)
+
+        while l <= r:
+            if values[m] <=
 
