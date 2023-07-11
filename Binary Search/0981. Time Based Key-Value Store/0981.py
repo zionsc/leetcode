9e951977bbe73808f190e1c5523ea2c8c994174b
Seeon Chang <zionchan@usc.edu>
2023-07-11 00:56:23 -0700

    0981

--- Binary Search/0981. Time Based Key-Value Store/0981.py
+++ Binary Search/0981. Time Based Key-Value Store/0981.py
@@ -1,9 +1,12 @@
 class TimeMap:
     def __init__(self):
+        # use defaultdict to initialize all non-initialized values as empty lists
         self.storage = defaultdict(list)
 
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        # for each key, add [value, timestamp] pair to it (append)
+        self.storage[key].append([value, timestamp])
 
 
     def get(self, key: str, timestamp: int) -> str:
