ad41f24d72cf36260d4b1b334c602fadcc9e0174
Seeon Chang <zionchan@usc.edu>
2023-07-11 00:55:14 -0700

    0981

---
+++ Binary Search/0981. Time Based Key-Value Store/0981.py
@@ -0,0 +1,10 @@
+class TimeMap:
+    def __init__(self):
+        self.storage = defaultdict(list)
+
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+
+
+    def get(self, key: str, timestamp: int) -> str:
+
