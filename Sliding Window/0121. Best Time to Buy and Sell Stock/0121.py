70205611c44741a7358649804b9ebf6ec69d8c94
Seeon Chang <zionchan@usc.edu>
2023-07-12 08:48:05 -0700

    0121

--- Sliding Window/0121. Best Time to Buy and Sell Stock/0121.py
+++ Sliding Window/0121. Best Time to Buy and Sell Stock/0121.py
@@ -10,3 +10,11 @@ class Solution:
                 maxProfit = max(maxProfit, prices[r] - prices[l])
 
             # negative value --> no profit
+            else:
+                # change l to r --> r that was found was the smallest value!
+                l = r
+            
+            # update r pointer to check next profit
+            r += 1
+
+        return maxProfit
