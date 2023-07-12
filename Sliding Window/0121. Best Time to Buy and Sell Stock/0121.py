c01798c135a11b39ed3b49ce2e8716663b0d2cf7
Seeon Chang <zionchan@usc.edu>
2023-07-12 08:45:43 -0700

    0121

---
+++ Sliding Window/0121. Best Time to Buy and Sell Stock/0121.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+
+        while r < len(prices):
+            # profit
+
+            # negative value --> no profit
