924a40fb214f1f2ad0b30a0809aeff692160b590
Seeon Chang <zionchan@usc.edu>
2023-07-15 14:10:49 -0700

    0076

--- Sliding Window/0076. Minimum Window Substring/0076.py
+++ Sliding Window/0076. Minimum Window Substring/0076.py
@@ -1,5 +1,15 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         # base case of when t is empty
-        if t = "":
+        if t == "":
             return ""
+
+        countT, countS = {}, {}
+
+        for i in t:
+            # if countT[i] does not exist, countT[i] will be initialized to 0 then the 1 will be added. otherwise can use a defaultdict(int)
+            countT = 1 + countT.get(i, 0)
+        
+        need, have = len(countT), 0
+        res, resLen = [-1, -1], float("inf")
+        l = 0
