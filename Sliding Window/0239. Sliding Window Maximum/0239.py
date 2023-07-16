8ae6a0693aa9876d27aac816df23c9ba2306dec4
Seeon Chang <zionchan@usc.edu>
2023-07-16 14:19:56 -0700

    0239

--- Sliding Window/0239. Sliding Window Maximum/0239.py
+++ Sliding Window/0239. Sliding Window Maximum/0239.py
@@ -10,7 +10,7 @@ class Solution:
 
         while r < len(nums):
 
-            # step 1: update the queue for the max value line-up
+            # step 1: update the queue for the max value line-up (right pointer)
             # we can pop the numbers from the back of the queue that are less than the current value that we are adding from the sliding window
             # we can do this because the value is the maximum, and it is the most recent one. Meaning that anything that was added before that is less than itself
             # does not need to be considered
@@ -19,13 +19,15 @@ class Solution:
             # now add to the position in the queue that it should be in to be considered "next-in-line" to be the max (or it already is if there is no next)
             q.append(r)
 
-            # step 2: remove the previous l pointer value if that was in consideration!
+            # step 2: remove the previous l pointer value if that was in consideration! (left pointer)
             # if q[0] is has now been removed from consideration (no longer in the sliding window!) -> because q holds the indexes, if l > q[0] that means we passed 
             # the previous max value -> the new window cannot have that max as the value anymore. 
             if l > q[0]:
                 q.popleft()
 
-            if 
+
+            # step 3: now update the output only if it is a valid sliding window (only at the start a few times until the window gets to the size of k)
+            if  
 
 
             output.append(q[-1])
