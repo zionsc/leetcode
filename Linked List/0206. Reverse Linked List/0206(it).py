dd6e4cce705a7b3481e2bed84c5e97b0b6449aa6
Seeon Chang <zionchan@usc.edu>
2023-07-16 15:09:00 -0700

    0206

--- Linked List/0206. Reverse Linked List/0206(it).py
+++ Linked List/0206. Reverse Linked List/0206(it).py
@@ -9,3 +9,11 @@ class Solution:
         # initialize these pointers (because or else it would edit the linked list values)
         prev, curr = None, head
 
+        # basic logic, just update the pointers. curr, curr.next, prev, temp are all POINTERS. NOT NODES THEMSELVES.
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        return prev
