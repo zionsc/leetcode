81e55f2d47f255d73eddc1153438539aef6226db
Seeon Chang <zionchan@usc.edu>
2023-07-16 14:10:07 -0700

    0239

--- Sliding Window/0239. Sliding Window Maximum/0239.py
+++ Sliding Window/0239. Sliding Window Maximum/0239.py
@@ -3,10 +3,15 @@ class Solution:
         output = []
 
         # creating a queue in python
-        q = collections.deque()
+        q = collections.deque() # contains the index of the value instead in order to check for out of bounds in sliding window, simply do nums[i] for value
 
         # left and right pointers in order to 
         l = r = 0
 
         while r < len(nums):
-            
+            # we can pop the numbers from the back of the queue that are less than the current value that we are adding from the sliding window
+            # we can do this because the value is the maximum, and it is the most recent one. Meaning that anything that was added before that is less than itself
+            # does not need to be considered
+            while q and nums[r] > nums[q[-1]]:
+                q.pop()
+            q.append(r)
