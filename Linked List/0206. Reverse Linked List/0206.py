48a81ffdad911ca78701d33dad4a69da01323d43
Seeon Chang <zionchan@usc.edu>
2023-07-16 14:52:10 -0700

    0206

--- Linked List/0206. Reverse Linked List/0206.py
+++ Linked List/0206. Reverse Linked List/0206.py
@@ -9,3 +9,11 @@ class Solution:
         if not head:
             return None
         
+        # we need to go backwards, but we also need to keep track of head.next! 
+        newHead = head
+        if head.next:
+            # go through to the end until the last value
+            newHead = self.reverseList(head.next)
+            # make head's next value point point to itself, essentially reversing that part of the linked list
+            head.next.next = head
+
