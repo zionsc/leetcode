581525f5a3eb1b7bc378b48e26c522c5169b56c7
Seeon Chang <zionchan@usc.edu>
2023-07-17 18:06:22 -0700

    0143

--- Linked List/0143. Reorder List/0143.py
+++ Linked List/0143. Reorder List/0143.py
@@ -3,4 +3,8 @@ class Solution:
 
         # step one: find the two halves of the linkedlist
         slow, fast = head, head.next
+        while fast and fast.next: # until fast and fast.next is out of bounds
+            slow = slow.next
+            fast = fast.next.next
+        
         
