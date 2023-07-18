01ba43f9f6c6325ed57bde096a693cd2ea22f76e
Seeon Chang <zionchan@usc.edu>
2023-07-18 13:43:11 -0700

    0019

--- Linked List/0019. Remove Nth Node From End of List/0019.py
+++ Linked List/0019. Remove Nth Node From End of List/0019.py
@@ -1,3 +1,22 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head) # set the dummy.next = head
+        l = dummy
+        r = head
+
+        # iterate r to have n nodes between r and l
+        while n > 0 and r:
+            r = r.next
+            n -=1
+        
+        # iterate until r goes out of bounds -> basically found the nth node from the end
+        while r:
+            l = l.next
+            r = r.next
+
+        # remove the nth node set l.next to the next.next value
+        l.next = l.next.next
+
+        return dummy.next
+
         
