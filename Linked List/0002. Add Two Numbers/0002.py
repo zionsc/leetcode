8b8e8d3178536a7c29256fba8918d68b9328fcdc
Seeon Chang <zionchan@usc.edu>
2023-07-19 11:51:18 -0700

    0002

--- Linked List/0002. Add Two Numbers/0002.py
+++ Linked List/0002. Add Two Numbers/0002.py
@@ -13,6 +13,14 @@ class Solution:
         # continue for or carry as well because of 8 + 7 -> then carry is 1, but then we forget about carry!
         carry = 0
         while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            val = v1 + v2 + carry
+
+            # update our val in order to make sure it doesnt go over 10 -> else carry!
+            carry = val % 10
+            val = val // 10
+
 
 
         
