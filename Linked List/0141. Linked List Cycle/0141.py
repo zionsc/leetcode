8e87a8f6d5a07dc5f5319acc10ac1cb9ecf46342
Seeon Chang <zionchan@usc.edu>
2023-07-20 12:51:09 -0700

    0141

---
+++ Linked List/0141. Linked List Cycle/0141.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # basically make sense of the fact that fast and fast.next cannot be nullptr because then it 
+        # eventually reaches an "end" -> a cycle would make sure it repeats forever.
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+            
+        return False
