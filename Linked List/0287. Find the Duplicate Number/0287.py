28499f58448ac4fb194a3322c23089160645e1bc
Seeon Chang <zionchan@usc.edu>
2023-07-20 13:28:18 -0700

    0287

--- Linked List/0287. Find the Duplicate Number/0287.py
+++ Linked List/0287. Find the Duplicate Number/0287.py
@@ -1,15 +1,3 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, 0
-        while True: # floyd's cycle detection algorithm here 
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break # slow == fast, there is a cycle --> the reason we do this is to make sure we find the cycle first
-
-        slow2 = 0
-        while True:
-            slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2: # slow == slow2, then there are two values that are the same that are not at the same index
-                return slow
+        
