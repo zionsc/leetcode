f893b37fbdb3946bdb6f09711aadf1da9f425368
Seeon Chang <zionchan@usc.edu>
2023-07-20 14:01:20 -0700

    0287

--- Linked List/0287. Find the Duplicate Number/0287.py
+++ Linked List/0287. Find the Duplicate Number/0287.py
@@ -3,3 +3,7 @@ class Solution:
         # basically find the cycle first, then have two pointers take one step at a time, essentially making sure
         # that if the two pointers are the same number, then that is a duplicate because they will never
         # be the same index -> as they are both taking one step at a time at different locations.
+        # the distance from 0 to the start of the cycle and the distance from the index that the the cycle
+        # algorithm found to the start of the cycle is the same, thus meaning that if they both take one step at
+        # a time, then they will both point to the start of the cycle at the same time, meaning that they will be
+        # duplicate numbers at different indexes (since they point to the same index) 
