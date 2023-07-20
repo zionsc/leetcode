d9df68856ccab49a0b0340782d8267cd9054d70f
Seeon Chang <zionchan@usc.edu>
2023-07-20 14:05:59 -0700

    0287

--- Linked List/0287. Find the Duplicate Number/0287.py
+++ Linked List/0287. Find the Duplicate Number/0287.py
@@ -7,9 +7,16 @@ class Solution:
         # algorithm found to the start of the cycle is the same, thus meaning that if they both take one step at
         # a time, then they will both point to the start of the cycle at the same time, meaning that they will be
         # duplicate numbers at different indexes (since they point to the same index) 
+        # must understand that values in the array are also indexes because it says values are from 1 ot n-1
 
         # step 1: cycle detection
         slow, fast = 0, 0
         while True:
             slow = nums[slow] # taking one step at a time
             fast = nums[nums[fast]] # taking two steps at once
+            if slow == fast:
+                break
+        
+
+        # step 2: find the duplicate by taking one step at a time from c-x and p.
+        
