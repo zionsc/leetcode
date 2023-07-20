e84f8a0360ed7c7d70c0db9dc2c3fadb8fce4613
Seeon Chang <zionchan@usc.edu>
2023-07-20 13:30:14 -0700

    0287

--- Linked List/0287. Find the Duplicate Number/0287.py
+++ Linked List/0287. Find the Duplicate Number/0287.py
@@ -1,3 +1,5 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+        # basically find the cycle first, then have two pointers take one step at a time, essentially making sure
+        # that if the two pointers are the same number, then that is a duplicate because they will never
+        # be the same index -> as they are both taking one step at a time at different locations.
