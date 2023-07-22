95299a151dcda72455e242125fa890cb0ce32be2
Seeon Chang <zionchan@usc.edu>
2023-07-21 22:32:09 -0700

    0023

--- Linked List/0023. Merge k Sorted Lists/0023.py
+++ Linked List/0023. Merge k Sorted Lists/0023.py
@@ -23,5 +23,24 @@ class Solution:
 
 
     def mergeLists(self, l1, l2):
-        # implement
+        # implement -> just merging two linkedlists
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val > l2.val:
+                tail.next = l2
+                l2 = l2.next
+            else:
+                tail.next = l1
+                l1 = l1.next
+            
+            tail = tail.next
         
+        if not l1:
+            tail.next = l2
+        if not l2:
+            tail.next = l1
+        
+        return dummy.next
+
