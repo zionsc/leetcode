42ef09665469bc44dd1f7b1b9bff7ec7dee2bb9f
Seeon Chang <zionchan@usc.edu>
2023-07-21 22:26:29 -0700

    0023

--- Linked List/0023. Merge k Sorted Lists/0023.py
+++ Linked List/0023. Merge k Sorted Lists/0023.py
@@ -10,10 +10,18 @@ class Solution:
         if not lists or len(lists) == 0:
             return None
         
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeLists(l1, l2))
+            lists = mergedLists
+
+        return lists[0]
         
-    
 
 
     def mergeLists(self, l1, l2):
         # implement
-        pass
+        
