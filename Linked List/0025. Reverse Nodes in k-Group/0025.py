cf87d7f95b2ab0b0fb53c5fe7ca67e527f6877da
Seeon Chang <zionchan@usc.edu>
2023-07-22 13:23:16 -0700

    0025

---
+++ Linked List/0025. Reverse Nodes in k-Group/0025.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # basic idea is to iterate until kth node does not exist -> reverse them is the issue, but that's simply
+        # messing around with pointers -> use a helperfunct to find kthnode
+
+        dummy = ListNode(0, head)
+
+        # must keep track of the prev node before the group to reverse as we need to update the pointers
+        # in order to continue the linkedlist
+        groupPrev = dummy
+
+        
