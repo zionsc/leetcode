32d2d07d179ae901805ef1fdaac211afe1b39f9f
Seeon Chang <zionchan@usc.edu>
2023-07-21 21:59:17 -0700

    0023

--- Linked List/0023. Merge k Sorted Lists/0023.py
+++ Linked List/0023. Merge k Sorted Lists/0023.py
@@ -7,6 +7,12 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
     
     # basic idea is to make a helper function that merges two lists -> merge two lists at a time until only one list left
+        if not lists or len(lists) == 0:
+            return None
+        
+        
+    
+
 
     def mergeLists(self, l1, l2):
         # implement
