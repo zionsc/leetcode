b28459a569a0dd71ecc24f3262477e7df63a7b6e
Seeon Chang <zionchan@usc.edu>
2023-07-24 09:30:04 -0700

    0110

---
+++ Trees/0110. Balanced Binary Tree/0110.py
@@ -0,0 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # must keep track of a height and a bool to check if node is balanced -> children must be balanced as well
