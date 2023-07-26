c638bada92cde6de3807aa602b83a332bae9afc2
Seeon Chang <zionchan@usc.edu>
2023-07-26 13:46:06 -0700

    0199

--- Trees/0199. Binary Tree Right Side View/0199.py
+++ Trees/0199. Binary Tree Right Side View/0199.py
@@ -6,4 +6,10 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        q = 
+        q = collections.deque()
+        res = []
+        q.append(root)
+
+        while q: # level-order traversal
+            for i in range(len(q)): # per level
+                node = q.popleft() # pop LEFT! -> pops until most very right (which is our rightmost node at that level)
