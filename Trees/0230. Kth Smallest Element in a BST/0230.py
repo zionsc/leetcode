6377b92401f67a6583eb064c623fab685aaaa8fd
Seeon Chang <zionchan@usc.edu>
2023-07-27 13:57:59 -0700

    0230

---
+++ Trees/0230. Kth Smallest Element in a BST/0230.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # recursive -> add to an array, sort the array, return array[k-1]
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # iterative -> go left until possible, when return back, pop from stack -> go right when you can't go left -> n+=1
+        # when n == k: return node.val
