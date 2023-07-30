549903bcc09a4002d31304946fe2418ca8e3429b
Seeon Chang <zionchan@usc.edu>
2023-07-30 16:23:40 -0700

    0297

--- Trees/0297. Serialize and Deserialize Binary Tree/0297.py
+++ Trees/0297. Serialize and Deserialize Binary Tree/0297.py
@@ -7,5 +7,18 @@
 
 class Codec:
     def serialize(self, root):
+        # recursively go through with a delimeter being whatever, but in this case: ","
+        res = []
+        def dfs(root):
+            if not root:
+                res.append("N")
+                return 
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        
+        dfs(root)
+        # appending it all to a string with each item being separated by ","
+        return ",".join(res)
 
     def deserialize(self, data):
