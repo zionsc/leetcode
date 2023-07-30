0da6ce0b42e1657ad66344645f714a892bf5e20a
Seeon Chang <zionchan@usc.edu>
2023-07-30 16:16:17 -0700

    0297

---
+++ Trees/0297. Serialize and Deserialize Binary Tree/0297.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root):
+
+    def deserialize(self, data):
