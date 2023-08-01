cbb0246575d9670a6d7f0153c4ff9c7541422228
Seeon Chang <zionchan@usc.edu>
2023-08-01 16:32:14 -0700

    0046

--- Backtracking/0046. Permutations/0046.py
+++ Backtracking/0046. Permutations/0046.py
@@ -4,3 +4,14 @@ class Solution:
 
         # basically recursively swap vals
         def backtrack(i):
+            if i >= len(nums):
+                res.append(nums.copy())
+            
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                backtrack(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+            
+        backtrack(0)
+        return res
