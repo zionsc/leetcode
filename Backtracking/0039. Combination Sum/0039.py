daed44faf3b06af7820abc41d95e50369ac69161
Seeon Chang <zionchan@usc.edu>
2023-07-31 23:38:05 -0700

    0039

--- Backtracking/0039. Combination Sum/0039.py
+++ Backtracking/0039. Combination Sum/0039.py
@@ -4,3 +4,10 @@ class Solution:
 
         # i to keep track of what index of candidates[i] we are at 
         def dfs(i, currSubset, currSum):
+            # base cases of true or backtrack!
+            if currSum == target:
+                res.append(currSubset)
+                return
+            if  i >= len(candidates) or currSum > target:
+                return 
+            
