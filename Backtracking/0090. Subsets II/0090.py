1c30d25ad44ac058c62f898610906a47bd2dc69e
Seeon Chang <zionchan@usc.edu>
2023-08-01 17:35:10 -0700

    0090

--- Backtracking/0090. Subsets II/0090.py
+++ Backtracking/0090. Subsets II/0090.py
@@ -1,2 +1,6 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        # must be sorted in order to skip duplicate numbers -> to skip duplicate replicate recursive trees
+        nums.sort()
