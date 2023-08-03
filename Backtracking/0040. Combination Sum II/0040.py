02a51ab28c1fa422ce0f9beccaf96f5fe4e95aa5
Seeon Chang <zionchan@usc.edu>
2023-08-02 22:30:53 -0700

    0040

--- Backtracking/0040. Combination Sum II/0040.py
+++ Backtracking/0040. Combination Sum II/0040.py
@@ -6,5 +6,11 @@ class Solution:
         def backtrack(i, currSubset, currSum):
             if currSum == target:
                 res.append(currSubset.copy()) # must do a copy since it is by reference all ways
+                return
+            if i >= len(candidates) or currSum > target:
+                return
+
+            # decision to add candidates[i]
+            
 
 
