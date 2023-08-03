8aa8cd9fb5facd6bf8258b1248511baf7fefcba0
Seeon Chang <zionchan@usc.edu>
2023-08-03 16:46:48 -0700

    0131

--- Backtracking/0131. Palindrome Partitioning/0131.py
+++ Backtracking/0131. Palindrome Partitioning/0131.py
@@ -8,3 +8,7 @@ class Solution:
                 # if we got to the end, that means we can just add whatever it added as a palindrome
                 res.append(currPartition.copy()) 
                 return
+            
+            for j in range(i, len(s)): # check all from 0 to the end in order to see palindrome
+                if self.isPalindrome(s, i, j):
+                    currPartition.append(s[i:j+1]) # j + 1 because inclusive:exclusive, so to include j.
