ec0a3a2ba395db4ddabb3606e85e672d20483660
Seeon Chang <zionchan@usc.edu>
2023-08-02 22:19:08 -0700

    0040

--- Backtracking/0040. Combination Sum II/0040.py
+++ Backtracking/0040. Combination Sum II/0040.py
@@ -1,3 +1,5 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
+        res = []
+        candidates.sort() # same as susbets II -> candidates must be sorted to skip duplicate numnbers!
+
