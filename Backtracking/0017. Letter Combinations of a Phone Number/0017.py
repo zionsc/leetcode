2bf4d054814aee5de0b9af212756052e834b5f44
Seeon Chang <zionchan@usc.edu>
2023-08-03 19:09:52 -0700

    0017

--- Backtracking/0017. Letter Combinations of a Phone Number/0017.py
+++ Backtracking/0017. Letter Combinations of a Phone Number/0017.py
@@ -1,2 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2" : "abc"
+            "3" : "def"
+            "4" : "ghi"
+            "5" : "jkl"
+            "6" : "mno"
+            "7" : "qprs"
+            "8" : "tuv"
+            "9" : "wxyz"
+        }
+
+        def backtrack(i, currStr):
+            if len(currStr) == len(digits):
+                
