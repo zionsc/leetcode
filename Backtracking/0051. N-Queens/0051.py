a23f052005bfd36120ad96853fe0813df614995e
Seeon Chang <zionchan@usc.edu>
2023-08-05 19:35:40 -0700

    0051

--- Backtracking/0051. N-Queens/0051.py
+++ Backtracking/0051. N-Queens/0051.py
@@ -13,4 +13,19 @@ class Solution:
                 copy = ["".join(row) for row in board]
                 res.append(copy)
             for c in range(n):
-                if c in cols or (r + c)
+                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                cols.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                
+                backtrack(r + 1)
+
+                cols.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+
+            
+        backtrack(0)
+        return res
