240837c9843ed46876f261dfe0bdd77916426775
Seeon Chang <zionchan@usc.edu>
2023-08-05 19:28:44 -0700

    0051

--- Backtracking/0051. N-Queens/0051.py
+++ Backtracking/0051. N-Queens/0051.py
@@ -2,8 +2,8 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         # basically iterate through rows, have a set for cols, posDiag, negDiag
         cols = set()
-        posDiag = set()
-        negDiag = set()
+        posDiag = set() # (r + c) topleft -> bottomright
+        negDiag = set() # (r - c) bottomleft -> topright
 
         res = []
         board =[["."] * n for i in range(n)] # n x for i in range(n) (cols)
@@ -11,3 +11,6 @@ class Solution:
         def backtrack(r):
             if r == n: # when we iterate through all the rows
                 copy = ["".join(row) for row in board]
+                res.append(copy)
+            for c in range(n):
+                if c in cols or (r + c)
