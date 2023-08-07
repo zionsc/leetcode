19365b88447a633dc03f101ea8d64efe4ef4c305
Seeon Chang <zionchan@usc.edu>
2023-08-06 17:01:30 -0700

    0070

--- 1D Dynamic Programming/0070. Climbing Stairs/0070.py
+++ 1D Dynamic Programming/0070. Climbing Stairs/0070.py
@@ -1,2 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+    
+        dp = [0] * n
+        dp[n-1] = 1 # can take 1 step to get to n
+        dp[n-2] = 2 # can take 1 step to get to n-1, or 2 step to get to n
+        
+
