5453979a40712f64c04ac8d905264e6c78dd7d22
Seeon Chang <zionchan@usc.edu>
2023-08-06 17:02:00 -0700

    0070

--- 1D Dynamic Programming/0070. Climbing Stairs/0070.py
+++ 1D Dynamic Programming/0070. Climbing Stairs/0070.py
@@ -6,5 +6,9 @@ class Solution:
         dp = [0] * n
         dp[n-1] = 1 # can take 1 step to get to n
         dp[n-2] = 2 # can take 1 step to get to n-1, or 2 step to get to n
-        
 
+
+        for i in range(n-3, -1, -1):
+            dp[i] = dp[i + 1] + dp[i + 2]
+
+        return dp[0]
