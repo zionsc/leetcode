bdef586af3920236d76ad84584ade530d6db75e5
Seeon Chang <zionchan@usc.edu>
2023-08-07 16:49:06 -0700

    0746

--- 1D Dynamic Programming/0746. Min Cost Climbing Stairs/0746.py
+++ 1D Dynamic Programming/0746. Min Cost Climbing Stairs/0746.py
@@ -3,3 +3,7 @@ class Solution:
 
         # basically leave the last two values in cost, and iterate backwards to determine what 
         # the min cost would be 
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        
+        return min(cost[0], cost[1])
