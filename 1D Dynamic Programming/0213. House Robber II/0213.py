bb15986b2d30dd2bb3c517d8b60e48fd234c7bde
Seeon Chang <zionchan@usc.edu>
2023-08-09 17:21:36 -0700

    0213

--- 1D Dynamic Programming/0213. House Robber II/0213.py
+++ 1D Dynamic Programming/0213. House Robber II/0213.py
@@ -2,6 +2,7 @@ class Solution:
     def rob(self, nums: List[int]) -> int:  
         # basically run house robber I twice on the nums[1:] and nums[:-1] (because we can't have the last and first values together selected)
 
+
     def helper(self, nums):
         rob1, rob2 = 0, 0
-        
+        # [rob1, rob2, n, n+1, ...]
