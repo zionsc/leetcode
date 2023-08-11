1b02681f0017c52332db84895aeae061585a4ab2
Seeon Chang <zionchan@usc.edu>
2023-08-10 20:11:06 -0700

    0005

--- 1D Dynamic Programming/0005. Longest Palindromic Substring/0005.py
+++ 1D Dynamic Programming/0005. Longest Palindromic Substring/0005.py
@@ -1,3 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+        res = ""
+        resLen = 0
+
+        # basically go through all possible start points within the string, start at that startpoint, expend outwards
+        # for each possible substring.
+        # we do not have to check whether it is odd or even because the palindrome checks will do our job for us
+
+        for i in range(len(s)):
+
+            # odd length palindromes
