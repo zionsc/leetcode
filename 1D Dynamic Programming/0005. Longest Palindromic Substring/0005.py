14ebe88c8996d0c13c2b70ea26160cf8e6ff0132
Seeon Chang <zionchan@usc.edu>
2023-08-10 20:13:35 -0700

    0005

--- 1D Dynamic Programming/0005. Longest Palindromic Substring/0005.py
+++ 1D Dynamic Programming/0005. Longest Palindromic Substring/0005.py
@@ -10,3 +10,11 @@ class Solution:
         for i in range(len(s)):
 
             # odd length palindromes
+            l, r = i, i # start at i and expand outwards
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resLen = r - l + 1
+                    res = s[l:r + 1]
+                    # expand outwards with the start location still being at i
+                    l -= 1
+                    r += 1 
