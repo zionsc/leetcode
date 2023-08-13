45d6e265dc5a76ecc03b03f4d286ab949e281070
Seeon Chang <zionchan@usc.edu>
2023-08-12 18:48:02 -0700

    0091

--- 1D Dynamic Programming/0091. Decode Ways/0091.py
+++ 1D Dynamic Programming/0091. Decode Ways/0091.py
@@ -9,4 +9,7 @@ class Solution:
             else:
                 dp[i] = dp[i + 1]
             
-            if 
+            if s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456":
+                dp[i] += dp[i + 2]
+            
+        return dp[0]
