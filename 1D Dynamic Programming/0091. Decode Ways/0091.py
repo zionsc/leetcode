6b747f2b41b4ce955b0ea3a28af3f25ffdb85e71
Seeon Chang <zionchan@usc.edu>
2023-08-12 18:47:07 -0700

    0091

--- 1D Dynamic Programming/0091. Decode Ways/0091.py
+++ 1D Dynamic Programming/0091. Decode Ways/0091.py
@@ -2,3 +2,11 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         dp = { len(s) : 1 }
         # you aren't adding the different ways up until it gets to dp[0], one way is simply one way to decode it.
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                return 0
+            else:
+                dp[i] = dp[i + 1]
+            
+            if 
