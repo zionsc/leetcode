e5de92be37fe27cbe181137cfc93dff5d61ec673
Seeon Chang <zionchan@usc.edu>
2023-08-13 05:26:45 -0700

    0322

--- 1D Dynamic Programming/0322. Coin Change/0322.py
+++ 1D Dynamic Programming/0322. Coin Change/0322.py
@@ -4,4 +4,8 @@ class Solution:
         dp[0] = 0
 
         for a in range(1, amount + 1): # since it is inclusive, exclusive
-            
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c]) # recurrence relation
+
+        return dp[amount] if dp[amount] != amount + 1 else -1 
