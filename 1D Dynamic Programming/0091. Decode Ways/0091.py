3d558935521a7fb2d85c1171dc2227af44caa38d
Seeon Chang <zionchan@usc.edu>
2023-08-12 18:03:09 -0700

    0091

--- 1D Dynamic Programming/0091. Decode Ways/0091.py
+++ 1D Dynamic Programming/0091. Decode Ways/0091.py
@@ -1,3 +1,4 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
+        dp = { len(s) : 1 }
+        # you aren't adding the different ways up until it gets to dp[0], one way is simply one way to decode it.
