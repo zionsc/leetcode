a8bb7b536e7f64ffc31c14e3217f2ce4df81cc19
Seeon Chang <zionchan@usc.edu>
2023-08-13 05:25:45 -0700

    0322

--- 1D Dynamic Programming/0322. Coin Change/0322.py
+++ 1D Dynamic Programming/0322. Coin Change/0322.py
@@ -1,3 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1] * (amount + 1) # we want amount + 1 to be our default value for all values from 0 - amount
+        dp[0] = 0
+
+        for a in range(1, amount + 1): # since it is inclusive, exclusive
+            
