2ff5df78f55024a14167e8bbc5c87e98a3f5f1ef
Seeon Chang <zionchan@usc.edu>
2023-08-14 18:09:20 -0700

    0152

--- 1D Dynamic Programming/0152. Maximum Product Subarray/0152.py
+++ 1D Dynamic Programming/0152. Maximum Product Subarray/0152.py
@@ -1,3 +1,5 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = max(res) # [-1] -> then res would be -1. we can't set it to a default val like 1 or 0.
+        currMin, currMax = 1, 1
+        # reason we also keep track of currMin is due to consecutive negative values -> -1 * -2 = 2
