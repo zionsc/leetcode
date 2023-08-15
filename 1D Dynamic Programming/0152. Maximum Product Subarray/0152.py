abeef9eb154b85c4069009910ff5d1f446756d57
Seeon Chang <zionchan@usc.edu>
2023-08-14 18:11:10 -0700

    0152

--- 1D Dynamic Programming/0152. Maximum Product Subarray/0152.py
+++ 1D Dynamic Programming/0152. Maximum Product Subarray/0152.py
@@ -13,3 +13,6 @@ class Solution:
             temp = currMax
             currMax = max(n * currMax, n * currMin, n)
             currMin = min(n * temp, n * currMin, n)
+            res = max(currMax, res)
+        
+        return res
