0ab8efe66b0bcd6321da0b419e3437e136434e36
Seeon Chang <zionchan@usc.edu>
2023-08-15 06:24:50 -0700

    0139

--- 1D Dynamic Programming/0139. Word Break/0139.py
+++ 1D Dynamic Programming/0139. Word Break/0139.py
@@ -6,4 +6,5 @@ class Solution:
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
                 if (i + len(w) <= len(s) and s[i : i + len(w)] == w):
-                    
+                    dp[i] = dp[i + len(w)] # to set it to true if the bottom-up last value was also set to true -> 
+                                           # caching the values as we do a bottom up approach
