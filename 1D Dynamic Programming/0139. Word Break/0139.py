d44dd8eaa30eda20db720e69aea72615dc3d359d
Seeon Chang <zionchan@usc.edu>
2023-08-15 06:21:48 -0700

    0139

--- 1D Dynamic Programming/0139. Word Break/0139.py
+++ 1D Dynamic Programming/0139. Word Break/0139.py
@@ -1,3 +1,7 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1) # because base case should be the one out of bounds in the length of s
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
