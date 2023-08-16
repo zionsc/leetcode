57a50905214c9a908e7796df25ace4b0ba5592a2
Seeon Chang <zionchan@usc.edu>
2023-08-16 10:52:40 -0700

    0300

---
+++ 1D Dynamic Programming/0300. Longest Increasing Subsequence/0300.py
@@ -0,0 +1,3 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # bottom-up DP approach: reason for that is because we want to check each possible subsequence
---
+++ 1D Dynamic Programming/0300. Longest Increasing Subsequence/README.md
@@ -0,0 +1,4 @@
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
+
