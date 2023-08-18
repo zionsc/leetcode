d5b5f82dba273836beaf4f50ab4841b41aee61dd
Seeon Chang <zionchan@usc.edu>
2023-08-17 17:19:15 -0700

    0416

--- 1D Dynamic Programming/0416. Partition Equal Subset Sum/0416.py
+++ 1D Dynamic Programming/0416. Partition Equal Subset Sum/0416.py
@@ -1,3 +1,3 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # 
+        # basically the sum must be divisble by two in order for the two subsets to have the same sum!
