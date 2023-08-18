4b2ee054b8ec1b862daeffffa8c40b0aed950537
Seeon Chang <zionchan@usc.edu>
2023-08-17 17:25:37 -0700

    0416

--- 1D Dynamic Programming/0416. Partition Equal Subset Sum/0416.py
+++ 1D Dynamic Programming/0416. Partition Equal Subset Sum/0416.py
@@ -5,8 +5,13 @@ class Solution:
             return False
         
         dp = set()
-        dp.add(0) # base case: if we don't add the first value in the iteration
+        dp.add(0) # base case: what if we add no value so far 
         target = sum(nums) // 2 # for two partitions to have the same sum, it must be an equal sum of sum(nums) // 2
 
         for i in range(len(nums) - 1, -1, -1):
+            newDP = dp.copy()
             for t in dp: # if at any point, the target comes up, it is possible to partition them!
+                newDP.add(i + t)
+            dp = newDP
+        
+        return 
