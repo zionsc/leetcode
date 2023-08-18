c24f72b217d8fd271b1cafcda02beaaa3b8a6ff5
Seeon Chang <zionchan@usc.edu>
2023-08-18 05:37:30 -0700

    0200

--- Graphs/0200. Number of Islands/0200.py
+++ Graphs/0200. Number of Islands/0200.py
@@ -1,2 +1,17 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        # basically we want to iterate through each [row][col] to see if it has been marked by bfs
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+
+        def bfs(r, c):
+            queue = collections.deque()
+            queue.append((r, c))
+            visited.add((r, c))
+            
+            while queue:
+                neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                
