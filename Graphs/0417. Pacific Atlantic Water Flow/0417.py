402bcc13f9bda855e5a408fb0848ceb5bd77966f
Seeon Chang <zionchan@usc.edu>
2023-08-22 18:26:11 -0700

    0417

--- Graphs/0417. Pacific Atlantic Water Flow/0417.py
+++ Graphs/0417. Pacific Atlantic Water Flow/0417.py
@@ -2,3 +2,18 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
         pac, atl = set(), set() # checking for each r, c if they are in both pac and atl
+        res = []
+
+        def dfs(r, c, visitSet, prevHeight):
+            # since we are starting from the border values, we must check that heights[r][c] < prevHeight as the base case to stop the recursive tree (opposite thinking)
+            if ((r, c) in visitSet or r < 0 or c < 0 or r >= rows or c >= cols or heights[r][c] < prevHeight):
+                return
+            
+            
+            visitSet.add((r, c))
+
+            # each recursive tree
+            dfs(r + 1, c, visitSet, heights[r][c])
+            dfs(r - 1, c, visitSet, heights[r][c])
+            dfs(r, c + 1, visitSet, heights[r][c])
+            dfs(r, c - 1, visitSet, heights[r][c])
