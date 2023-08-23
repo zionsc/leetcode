23814ef9cb20b9d5d1306f72e608158615112509
Seeon Chang <zionchan@usc.edu>
2023-08-23 08:40:44 -0700

    0130

--- Graphs/0130. Surrounded Regions/0130.py
+++ Graphs/0130. Surrounded Regions/0130.py
@@ -5,3 +5,8 @@ class Solution:
         """
         rows, cols = len(board), len(board[0])
         visitSet = set()
+
+        def bfs(r, c):
+            if r not in range(rows) or c not in range(cols) or (r, c) in visitSet or board[r][c] == "X":
+                return
+            
