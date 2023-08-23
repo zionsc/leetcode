fa1f020a33fe2e69bb05751eead3bf2c14e97da3
Seeon Chang <zionchan@usc.edu>
2023-08-22 18:22:56 -0700

    0417

---
+++ Graphs/0417. Pacific Atlantic Water Flow/0417.py
@@ -0,0 +1,4 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        pac, atl = set(), set() # checking for each r, c if they are in both pac and atl
