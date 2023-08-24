26fa033fd799b92e58e6733444b1b7e85a969ee1
Seeon Chang <zionchan@usc.edu>
2023-08-24 08:17:33 -0700

    0994

--- Graphs/0994. Rotting Oranges/0994.py
+++ Graphs/0994. Rotting Oranges/0994.py
@@ -1,3 +1,8 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
+        rows, cols = len(grid), len(grid[0])
+        q = collections.deque()
+        time, fresh = 0, 0
+
+        # basic idea is because we may have more than one rotten orange, they will start making the fresh ones rotten at the same time! -> reason for queue instead
+        # of simple BFS.
