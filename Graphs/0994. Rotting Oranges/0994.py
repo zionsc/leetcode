5dc9ff91969659e3975dc664e3f4c9241b84789f
Seeon Chang <zionchan@usc.edu>
2023-08-24 08:20:36 -0700

    0994

--- Graphs/0994. Rotting Oranges/0994.py
+++ Graphs/0994. Rotting Oranges/0994.py
@@ -6,3 +6,15 @@ class Solution:
 
         # basic idea is because we may have more than one rotten orange, they will start making the fresh ones rotten at the same time! -> reason for queue instead
         # of simple BFS.
+
+        # first finding the number of fresh oranges (if not connected to the group that contains rotten, we cannot get every orange rotten!)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1: # fresh
+                    fresh += 1
+                elif grid[r][c] == 2: # rotten
+                    q.append([r, c])
+
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        
