d185a56c36690cdb856f59b2d81a4a894bc1ea6e
Seeon Chang <zionchan@usc.edu>
2023-08-25 08:51:31 -0700

    0207

--- Graphs/0207. Course Schedule/0207.py
+++ Graphs/0207. Course Schedule/0207.py
@@ -1,3 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = { i:[] for i in range(numCourses) } # hashmap for each course
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        visitSet = set()
+
         
