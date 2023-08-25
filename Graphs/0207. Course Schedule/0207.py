b354506bf3c1c09c113ff2ad180e8467a1c872ae
Seeon Chang <zionchan@usc.edu>
2023-08-25 08:53:52 -0700

    0207

--- Graphs/0207. Course Schedule/0207.py
+++ Graphs/0207. Course Schedule/0207.py
@@ -4,6 +4,22 @@ class Solution:
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
         
+        # to determine cycles
         visitSet = set()
 
-        
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            # if it did not catch false at any point in the loop (no cycle), it means that THAT course is actually able to be taken (no course cycle paradox)
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+
