59b841bfea4baad12e66222d4bbd760320c43d53
Seeon Chang <zionchan@usc.edu>
2023-08-27 01:20:55 -0700

    0210

--- Graphs/0210. Course Schedule II/0210.py
+++ Graphs/0210. Course Schedule II/0210.py
@@ -1,2 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = { c:[] for c in range(numCourses) }
+
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        output = []
+        visit, cycle = set(), set()
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+            
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+        return output
+
+
+
