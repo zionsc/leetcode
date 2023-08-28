a53f0a28342c5880c40710fb642e296d602ddf1f
Seeon Chang <zionchan@usc.edu>
2023-08-28 11:39:10 -0700

    0684

--- Graphs/0684. Redundant Connection/0684.py
+++ Graphs/0684. Redundant Connection/0684.py
@@ -1,3 +1,3 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        
+        parent = [i for i in range(len(edges) + 1)] # each node is going to be its parent for now. (start at 0 for the math to make sense, but we won't use it.)
