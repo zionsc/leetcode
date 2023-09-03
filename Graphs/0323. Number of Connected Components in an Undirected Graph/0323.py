d85e52897df0826b50fe6b434ea31cb2674b08fc
Seeon Chang <zionchan@usc.edu>
2023-09-02 20:05:30 -0700

    0323

--- Graphs/0323. Number of Connected Components in an Undirected Graph/0323.py
+++ Graphs/0323. Number of Connected Components in an Undirected Graph/0323.py
@@ -9,6 +9,13 @@ class Solution:
             while res != parent[res]:
                 res = parent[parent[res]] # path compression (just simple optimization)
                 res = parent[res]
-                
+
             return res
         
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return 0
+            
+            
