75a1e41a1c301f088bdf6525fd7415bac73a4166
Seeon Chang <zionchan@usc.edu>
2023-09-02 20:04:00 -0700

    0323

---
+++ Graphs/0323. Number of Connected Components in an Undirected Graph/0323.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            res = n1
+
+            while res != parent[res]:
+                res = parent[res]
+
+            return res
+        
+        
