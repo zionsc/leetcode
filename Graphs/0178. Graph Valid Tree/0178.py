5c6ade1bbd08937f7e16aee29e03720f6c364431
Seeon Chang <zionchan@usc.edu>
2023-09-05 11:32:12 -0700

    0178

---
+++ Graphs/0178. Graph Valid Tree/0178.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+        
