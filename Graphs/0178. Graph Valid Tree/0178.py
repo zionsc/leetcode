17118c29327077aa9d19ee89204a869e063c4129
Seeon Chang <zionchan@usc.edu>
2023-09-05 14:07:47 -0700

    0138

--- Graphs/0178. Graph Valid Tree/0178.py
+++ Graphs/0178. Graph Valid Tree/0178.py
@@ -5,29 +5,23 @@ class Solution:
     @return: true if it's a valid tree, or false
     """
     def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+        # In the case that there are no nodes, then it is true!
         if not n:
             return True
-
+        
+        # to check for loops
+        visited = set()
         adjacencyList = { i:[] for i in range(n) }
+
         for n1, n2 in edges:
             adjacencyList[n1].append(n2)
             adjacencyList[n2].append(n1)
-
-        visited = set()
+        
         def dfs(node, prev):
-            if node in visited:
+            if node in visited: # cycle
                 return False
             
-            visited.add(node)
-            for j in adjacencyList[node]:
-                if j == prev:
-                    continue
-                if not dfs(j, node):
-                    return False
-            
-            return True
         
-        return dfs(0, -1) and n == len(visit)
 
             
 
--- Linked List/0138. Copy List with Random Pointer/0138.py
+++ Linked List/0138. Copy List with Random Pointer/0138.py
@@ -10,7 +10,7 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         # if the old one is none, it should create a copy as none as well. 
-        oldToCopy = { None: None }
+        oldToCopy = { None:None }
 
         # first iteration to create the deepcopy
         curr = head
