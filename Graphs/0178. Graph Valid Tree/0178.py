30bdcd780432c2c812a334151ba6b6aae2bbb26a
Seeon Chang <zionchan@usc.edu>
2023-09-05 12:53:17 -0700

    0178

--- Graphs/0178. Graph Valid Tree/0178.py
+++ Graphs/0178. Graph Valid Tree/0178.py
@@ -8,4 +8,25 @@ class Solution:
         if not n:
             return True
 
-        adjacencyList = { i: }
+        adjacencyList = { i:[] for i in range(n) }
+        for n1, n2 in edges:
+            adjacencyList[n1].append(n2)
+            adjacencyList[n2].append(n1)
+
+        visited = set()
+        def dfs(node, prev):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+            for j in adjacencyList[node]:
+                if j == prev:
+                    continue
+                if not dfs(j, node):
+                    return False
+            
+            return True
+
+            
+
+            
