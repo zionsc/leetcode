04339f0aeae62d6db62e72d0a665677829bc56e6
Seeon Chang <zionchan@usc.edu>
2023-09-11 17:19:28 -0700

    0127

--- Graphs/0127. Word Ladder/0127.py
+++ Graphs/0127. Word Ladder/0127.py
@@ -1,3 +1,14 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # 1. make the adj. List
+        # 2. use bfs --> each layer: res += 1
+
+        if endWord not in wordList:
+            return 0 
         
+        nei = defaultdict(list) # if not there, initalize it first with an empty lis
+        
+        for word in wordList:
+            for i in word:
+                pattern = word[:i] + "*" + word[i + 1:] # for each pattern, (*ot, h*t, ho*), 1 letter difference
+                nei[pattern].append(word) # add that to the defaultdict
