c0c6ba8d69b01824c6f0432d83e29f6139802409
Seeon Chang <zionchan@usc.edu>
2023-09-12 13:09:52 -0700

    1046

--- Heap Priority Queue/1046. Last Stone Weight/1046.py
+++ Heap Priority Queue/1046. Last Stone Weight/1046.py
@@ -1,4 +1,11 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         # in order to create a max heap in python, minheap values must be negated, thus flipping the minheap into a max heap.
-        
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = -heapq.heappop(stones) # x is the first value popped from our max heap, thus it is the larger value.
+            second = -heapq.heappop(stones)
+            if first != second:
+                heapq.heappush(stones, -(first - second)) # must negate it again to maintain max_heap property
