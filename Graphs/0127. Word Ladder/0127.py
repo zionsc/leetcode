c39e8d023813f86c8e772d58c648c28158d0a8bd
Seeon Chang <zionchan@usc.edu>
2023-09-11 17:20:21 -0700

    0127

--- Graphs/0127. Word Ladder/0127.py
+++ Graphs/0127. Word Ladder/0127.py
@@ -6,9 +6,12 @@ class Solution:
         if endWord not in wordList:
             return 0 
         
+        wordList.append(beginWord) # must add it to wordlist in order to start from there 
         nei = defaultdict(list) # if not there, initalize it first with an empty lis
         
         for word in wordList:
             for i in word:
                 pattern = word[:i] + "*" + word[i + 1:] # for each pattern, (*ot, h*t, ho*), 1 letter difference
                 nei[pattern].append(word) # add that to the defaultdict
+
+        q = collections.deque([beginWord])
