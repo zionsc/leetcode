04fa12685febb4c3dcf303f54db31a595d42374e
Seeon Chang <zionchan@usc.edu>
2023-09-12 00:14:22 -0700

    0703

--- Heap Priority Queue/0703. Kth Largest Element in a Stream/0703.py
+++ Heap Priority Queue/0703. Kth Largest Element in a Stream/0703.py
@@ -11,6 +11,10 @@ class KthLargest:
         heapq.heappush(self.minHeap, val) # this is how to add to a heap in python
         if len(self.minHeap) > self.k: # in order to preserve the size of the minHeap as k, we must check if now the minHeap has gone over the size limit
             heapq.heappop(self.minHeap) 
+        return self.minHeap[0] # minHeap keeps the minimum value at the root (self.minHeap[0])
+        # the minimnum value in our minHeap is the kth largest value as we count down from the largest value down
+        # to the kth largest value, it is the last value in our count --> kth value in our count. This is the smallest
+        # value in our entire minHeap as our minHeap is kept as a size of k 
 
 
 # Your KthLargest object will be instantiated and called as such:
