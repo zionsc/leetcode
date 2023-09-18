64a7d6063c64c4c9d24b25dc2b402d59144d02ab
Seeon Chang <zionchan@usc.edu>
2023-09-18 11:59:29 -0700

    0973

--- Heap Priority Queue/0973. K Closest Points to Origin/0973.py
+++ Heap Priority Queue/0973. K Closest Points to Origin/0973.py
@@ -1,2 +1,7 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # can simply just use a minheap, the first value in each list element is the comparator
+        minHeap = []
+        res = []
+
+        
