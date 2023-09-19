35d6fc14484bfce8fe7a2e6897e715d4bc4b355b
Seeon Chang <zionchan@usc.edu>
2023-09-18 20:08:05 -0700

    0215

--- Heap Priority Queue/0215. Kth Largest Element in an Array/0215.py
+++ Heap Priority Queue/0215. Kth Largest Element in an Array/0215.py
@@ -1,5 +1,6 @@
 class Solution: 
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        currVal = max(nums)
         # must negate values in order to create a maxHeap
         maxHeap = [-i for i in nums]
         heapq.heapify(maxHeap)
