5a443e444f287c94c4928501f602ea84ab8e559d
Seeon Chang <zionchan@usc.edu>
2023-09-20 23:26:58 -0700

    0621

--- Heap Priority Queue/0621. Task Scheduler/0621.py
+++ Heap Priority Queue/0621. Task Scheduler/0621.py
@@ -2,4 +2,6 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         # basically gives me a hashmap [character, number of occurences within tasks]
         count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count] # this is how you create maxheaps in python! --> no library maxheap function
+        q = deque() # in order to keep track of tasks that are idle, but still need to be completed
         
