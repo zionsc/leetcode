d1fb8b6760d269dce33ff35738e162f14cd7ef86
Seeon Chang <zionchan@usc.edu>
2023-09-20 23:27:26 -0700

    0621

--- Heap Priority Queue/0621. Task Scheduler/0621.py
+++ Heap Priority Queue/0621. Task Scheduler/0621.py
@@ -3,5 +3,9 @@ class Solution:
         # basically gives me a hashmap [character, number of occurences within tasks]
         count = Counter(tasks)
         maxHeap = [-cnt for cnt in count] # this is how you create maxheaps in python! --> no library maxheap function
+        heapq.heapify(maxHeap)
         q = deque() # in order to keep track of tasks that are idle, but still need to be completed
-        
+
+        time = 0
+
+        while maxHeap
