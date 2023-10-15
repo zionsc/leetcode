a64b8403ef045cccfd3c8bfdc1ee32db71d10f2a
Seeon Chang <zionchan@usc.edu>
2023-10-14 23:18:43 -0700

    2742

--- 1D Dynamic Programming/2742. Painting the Walls/2742.py
+++ 1D Dynamic Programming/2742. Painting the Walls/2742.py
@@ -1,4 +1,9 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         # checking if the free painter could have painted up to all the walls up to j while the paid painter was doing i.
-        
+        money = [float('inf')] * (len(cost) + 1)
+        money[0] = [0]
+
+        for i in range(len(cost)):
+            for j in range(len(cost), -1, -1):
+                
