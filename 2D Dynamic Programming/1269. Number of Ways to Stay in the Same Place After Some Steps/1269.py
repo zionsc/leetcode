7c68063b70f3a70778d10e320883b4ce2402763c
Seeon Chang <zionchan@usc.edu>
2023-10-15 17:31:51 -0700

    1269

--- 2D Dynamic Programming/1269. Number of Ways to Stay in the Same Place After Some Steps/1269.py
+++ 2D Dynamic Programming/1269. Number of Ways to Stay in the Same Place After Some Steps/1269.py
@@ -2,3 +2,6 @@ class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
         mod = (10 ** 9) + 7
         maxLen = min(arrLen, (steps // 2) + 1)
+
+        dp = [[0] * maxLen for i in range(steps + 1)]
+        dp[0][0] = 1
