ee7fcfada7f4c48742114e119d64e527200d0ee1
Seeon Chang <zionchan@usc.edu>
2023-10-15 18:29:22 -0700

    0119

--- 1D Dynamic Programming/0119. Pascal's Triangle II/0119.py
+++ 1D Dynamic Programming/0119. Pascal's Triangle II/0119.py
@@ -1,2 +1,19 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        
+        prev = [1]
+
+        for i in range(1, rowIndex + 1):
+            curr = [1] # every row starts with 1
+
+            for j in range(1, len(prev)):
+                curr.append(prev[j -1] + prev[j])
+            
+            curr.append(1) # every row ends with 1
+            prev = curr
+
+        return prev
+
+        
