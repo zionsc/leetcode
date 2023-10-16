92634c5ec1bcc8a2cd229da89039c4dbf3bd0300
Seeon Chang <zionchan@usc.edu>
2023-10-15 18:29:40 -0700

    0119

--- 1D Dynamic Programming/0119. Pascal's Triangle II/0119.py
+++ 1D Dynamic Programming/0119. Pascal's Triangle II/0119.py
@@ -9,7 +9,7 @@ class Solution:
             curr = [1] # every row starts with 1
 
             for j in range(1, len(prev)):
-                curr.append(prev[j -1] + prev[j])
+                curr.append(prev[j - 1] + prev[j])
             
             curr.append(1) # every row ends with 1
             prev = curr
