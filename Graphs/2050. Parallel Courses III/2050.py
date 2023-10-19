9e75e5d41bf128b7d0e49aa2fd1f5b4ae7141ee9
Seeon Chang <zionchan@usc.edu>
2023-10-19 01:11:45 -0700

    2050

--- Graphs/2050. Parallel Courses III/2050.py
+++ Graphs/2050. Parallel Courses III/2050.py
@@ -1,3 +1,30 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        # adj list
+        adj_list = defaultdict(list)
+        in_degree = [0] * (n + 1)
+
+        for preq, crs in relations:
+            adj_list[preq].append(crs)
+            in_degree[crs] += 1
+
+        queue = deque()
+
+        for i in range(1, n + 1):
+            if in_degree[i] == 0:
+                queue.append(i)
+
+        topo_order = []
+
+        while queue:
+            crs = queue.pop()
+            topo_order.append(crs)
+            for nei in adj_list[crs]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+
+            
+
+
         
