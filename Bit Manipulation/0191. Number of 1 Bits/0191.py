5944632ef445de3c2efcad299e27b71eb091620a
Seeon Chang <zionchan@usc.edu>
2023-10-19 13:37:41 -0700

    0191

--- Bit Manipulation/0191. Number of 1 Bits/0191.py
+++ Bit Manipulation/0191. Number of 1 Bits/0191.py
@@ -1,2 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        for i in range(32):
+            if ((n >> i) & 1) == 1: # bit shift each position and & with 1 to mask it --> see if it is 1.
+                cnt += 1
+        return cnt
