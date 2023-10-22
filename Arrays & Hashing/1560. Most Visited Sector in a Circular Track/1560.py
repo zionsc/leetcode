0087bd36b2c1c61874a4b28f6a281b69eca75e54
Seeon Chang <zionchan@usc.edu>
2023-10-21 23:30:47 -0700

    1560

--- Arrays & Hashing/1560. Most Visited Sector in a Circular Track/1560.py
+++ Arrays & Hashing/1560. Most Visited Sector in a Circular Track/1560.py
@@ -1,2 +1,11 @@
 class Solution:
     def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
+        # intialize to the first and last sectors in rounds array
+        a, b = rounds[0], rounds[-1]
+        
+        # meaning we NEVER CROSSED the start and started again
+        if a <= b: 
+            return [i for i in range(a, b + 1)]
+        # meaning we DID CROSS the start and started again
+        else:
+            return [i for i in range(1, b + 1)] + [i for i in range(a, n + 1)]
