8f6259f807ee7c3cde61c67a4f1c4c1bf609fa5d
Seeon Chang <zionchan@usc.edu>
2023-10-22 00:13:41 -0700

    0038

--- Recursion/0038. Count and Say/0038.py
+++ Recursion/0038. Count and Say/0038.py
@@ -1,3 +1,19 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        
+        if n == 1:
+            return 1
+        res = "1"
+
+        def recursion(numStr):
+            nonlocal res
+            i = 0
+            ret = ""
+            while i < len(numStr):
+                count = 1
+                curr = numStr[i]
+                while 
+
+
+        for i in range(n - 1):
+            recursion(res)
+        return res
