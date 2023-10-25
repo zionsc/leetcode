238f9fcb6a8316c3c5030199e2375203ddf2661d
Seeon Chang <zionchan@usc.edu>
2023-10-24 18:06:05 -0700

    0779

--- Bit Manipulation/0779. K-th Symbol in Grammar/0779.py
+++ Bit Manipulation/0779. K-th Symbol in Grammar/0779.py
@@ -3,4 +3,8 @@ class Solution:
         if n == 1:
             return 0
         
-        
+        # basically gets the parent of the number we are looking for as --> two numbers come from each parent.
+        # The k-th position in the n-th row is derived from the (k+1)//2-th position in the (n-1)-th row. 
+        # 1st and 2nd positions in the n-th row come from the 1st position in the (n-1)-th row.
+        # 3rd and 4th positions in the n-th row come from the 2nd position in the (n-1)-th row.
+        parent = self.kthGrammar(n - 1, (k + 1) // 2)
