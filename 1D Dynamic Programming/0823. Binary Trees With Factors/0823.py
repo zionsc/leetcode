59c848021afae01c34000be6e5b3f0930d67431e
Seeon Chang <zionchan@usc.edu>
2023-10-25 19:36:01 -0700

    0823

--- 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
+++ 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
@@ -1,6 +1,7 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         MOD = 10**9 + 7
-        #
+        # the reason we sort is because we would break out of the loop earlier since we want to check i for being the parent
+        # and j for being a possible child nodde
         arr.sort()
         
