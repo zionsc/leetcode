d8bf7bace4b35cf02a1a7084c52419465b2e45e7
Seeon Chang <zionchan@usc.edu>
2023-10-25 22:26:35 -0700

    0823

--- 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
+++ 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
@@ -7,8 +7,8 @@ class Solution:
         my_set = set()
         dp = { x : 1 for x in arr }
 
-        for i in range(len(arr)):
-            for j in range(len(arr)):
+        for i in arr:
+            for j in arr:
                 if j > i**0.5: # if at any point j > sqrt(i) then break, move onto next i (36 --> 6, nothing above 6 can make 36)
                     break
 
