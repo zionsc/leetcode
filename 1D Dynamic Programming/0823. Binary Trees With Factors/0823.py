ed0c7fffaf3a608450f96e22eaca94dd3b6cd8e9
Seeon Chang <zionchan@usc.edu>
2023-10-25 22:22:13 -0700

    0823

--- 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
+++ 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
@@ -4,10 +4,24 @@ class Solution:
         # the reason we sort is because we would break out of the loop earlier since we want to check i for being the parent
         # and j for being a possible child nodde
         arr.sort()
-        s = set()
+        my_set = set()
         dp = { x : 1 for x in arr }
 
         for i in range(len(arr)):
             for j in range(len(arr)):
-                
+                if j > i**0.5: # if at any point j > sqrt(i) then break, move onto next i (36 --> 6, nothing above 6 can make 36)
+                    break
+
+                if i // j in my_set and i % j == 0: # if the value is in the set, and also if j is a factor of i. (basically is it possible to be a child?)
+                    if i // j == j: # j * j == i
+                        dp[i] += dp[j] * dp[j]
+                    else: # j * something == i
+                        dp[i] += dp[j] * dp[i // j]
+                    dp[i] %= MOD
+        return sum(dp.values()) % MOD
+
+
+
+
+
         
