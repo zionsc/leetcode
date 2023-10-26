e5acce9ff276dd0284fb6cdf6f20a8feabaabf9b
Seeon Chang <zionchan@usc.edu>
2023-10-25 19:36:46 -0700

    0823

--- 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
+++ 1D Dynamic Programming/0823. Binary Trees With Factors/0823.py
@@ -4,4 +4,6 @@ class Solution:
         # the reason we sort is because we would break out of the loop earlier since we want to check i for being the parent
         # and j for being a possible child nodde
         arr.sort()
+        s = set()
+        dp = { x : 1 for x in arr }
         
