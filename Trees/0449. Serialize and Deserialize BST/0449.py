1d5cfbc694d4ca833b2011eab4b1667cf7826ddc
Seeon Chang <zionchan@usc.edu>
2023-10-25 23:18:38 -0700

    0449

---
+++ Trees/0449. Serialize and Deserialize BST/0449.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string."""
+        if not root:
+            return ""
+        
+        res = ""
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            if not node:
+                
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree."""
+
+    
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
