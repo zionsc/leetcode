9c971e2854cc62d8199f328dc4612f309324bda1
Seeon Chang <zionchan@usc.edu>
2023-10-31 15:32:10 -0700

    1143

--- 2D Dynamic Programming/1143. Longest Common Subsequence/1143.py
+++ 2D Dynamic Programming/1143. Longest Common Subsequence/1143.py
@@ -4,4 +4,10 @@ class Solution:
 
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len(text2) - 1, -1, -1):
-                
+                if text1[i + 1] == text2[j + 1]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else: 
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+        
