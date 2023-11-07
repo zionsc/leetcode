c2ec4b419c81f9c2dc8b2e84a6b8002f9ba59d23
Seeon Chang <zionchan@usc.edu>
2023-11-06 18:17:42 -0800

    1921

--- Greedy/1921. Eliminate Maximum Number of Monsters/1921.py
+++ Greedy/1921. Eliminate Maximum Number of Monsters/1921.py
@@ -1,3 +1,10 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
         time = [ d / s for d, s in zip(dist, speed) ]
+        time.sort()
+
+        for i in range(len(time)):
+            if time[i] <= i:
+                return i
+        
+        return len(time)
