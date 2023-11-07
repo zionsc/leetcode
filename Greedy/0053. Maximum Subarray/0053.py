0ed0ae45b3c30602cdc1f0489e681837392cd387
Seeon Chang <zionchan@usc.edu>
2023-11-07 12:47:34 -0800

    0053

--- Greedy/0053. Maximum Subarray/0053.py
+++ Greedy/0053. Maximum Subarray/0053.py
@@ -1,2 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = curr_sum = float('-inf')
+        for i in range(len(nums)):
+            if curr_sum < 0:
+                curr_sum = nums[i]
+            else:
+                curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
