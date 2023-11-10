84b31295cb042ace3cc6e6539c4ec0bc840c1ad2
Seeon Chang <zionchan@usc.edu>
2023-11-09 18:32:16 -0800

    1743

--- Graphs/1743. Restore the Array From Adjacent Pairs/1743.py
+++ Graphs/1743. Restore the Array From Adjacent Pairs/1743.py
@@ -1,2 +1,7 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        for val1, val2 in adjacentPairs:
+            adj[val1].append(val2)
+            adj[val2].append(val1)
+            
