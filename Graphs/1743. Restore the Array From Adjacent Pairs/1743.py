1c05848fc7ea1ef8cae92b67af8e94cd69c7b88a
Seeon Chang <zionchan@usc.edu>
2023-11-09 18:34:51 -0800

    1743

--- Graphs/1743. Restore the Array From Adjacent Pairs/1743.py
+++ Graphs/1743. Restore the Array From Adjacent Pairs/1743.py
@@ -4,4 +4,10 @@ class Solution:
         for val1, val2 in adjacentPairs:
             adj[val1].append(val2)
             adj[val2].append(val1)
-            
+        
+        start_node = None
+        for i, v in adj.items():
+            if len(v) == 1:
+                start_node = i
+                break
+        
