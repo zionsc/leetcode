3e49b08cb99aea4415d457478a5f8f1c289aea3f
Seeon Chang <zionchan@usc.edu>
2023-11-10 23:25:12 -0800

    2642

---
+++ Graphs/2642. Design Graph With Shortest Path Calculator/2642.py
@@ -0,0 +1,6 @@
+class Graph:
+    def __init__(self, n: int, edges: List[List[int]]):
+
+    def addEdge(self, edge: List[int]) -> None:
+    
+    def shortestPath(self, node1: int, node2: int) -> int:
