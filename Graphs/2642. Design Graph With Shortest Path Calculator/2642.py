d2903e72fb41e4f9e39f2e8d6a034dfa963409e0
Seeon Chang <zionchan@usc.edu>
2023-11-10 23:29:58 -0800

    2642

--- Graphs/2642. Design Graph With Shortest Path Calculator/2642.py
+++ Graphs/2642. Design Graph With Shortest Path Calculator/2642.py
@@ -3,8 +3,19 @@ class Graph:
         self.adj = [[] for i in range(n)]
 
         for edge in edges:
-            
+            self.adj[edge[0]].append((edge[1], edge[2]))
 
     def addEdge(self, edge: List[int]) -> None:
+        self.adj[edge[0]].append((edge[1], edge[2]))
     
     def shortestPath(self, node1: int, node2: int) -> int:
+        return self.dijkstras(node1, node2)
+    
+    def dijkstras(self, node1: int, node2: int) -> int:
+        minHeap = [(node1, 0)] # priority queue for dijkstras
+        distances = [float('inf')] * len(self.adj)
+        distances[node1] = 0
+
+        while minHeap:
+            
+
