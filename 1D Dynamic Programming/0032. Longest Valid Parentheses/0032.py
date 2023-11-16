014270796e51e2cca5428f372ea281cfca2f3747
Seeon Chang <zionchan@usc.edu>
2023-11-16 01:22:50 -0800

    0032

--- 1D Dynamic Programming/0032. Longest Valid Parentheses/0032.py
+++ 1D Dynamic Programming/0032. Longest Valid Parentheses/0032.py
@@ -10,3 +10,7 @@ class Solution:
                 if s[i - 1] == '(':
                     dp[i] = dp[i - 2] + 2
                 elif i - dp[i - 1] > 0 and s[i - dp[i - 1] - 1] == '(': # check if longer recursive pattern
+                    dp[i] = dp[i - 1] + 2 + (dp[i - dp[i - 1] - 2] if i - dp[i - 2] >= 2 else 0) # must check if previous pattern exists --> if it does, add it.
+            max_len = max(max_len, dp[i])
+        
+        return max_len
