ae824ec7fbfd21331717bc496ac0442237268c1b
Seeon Chang <zionchan@usc.edu>
2023-11-15 19:29:14 -0800

    1980

--- Bit Manipulation/1980. Find Unique Binary String/1980.py
+++ Bit Manipulation/1980. Find Unique Binary String/1980.py
@@ -1,8 +1,15 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        set_nums = set(int(num, 2) for num in nums)
-        n = len(nums)
-        for i in range(n + 1):
-            if i not in set_nums:
-                return bin(i)[2:].zfill(n)
+        # set_nums = set(int(num, 2) for num in nums)
+        # n = len(nums)
+        # for i in range(n + 1):
+        #     if i not in set_nums:
+        #         return bin(i)[2:].zfill(n)
+        res = []
+        for i in range(len(nums)):
+            if nums[i][i] == '1':
+                res.append('0')
+            else:
+                res.append('1')
+        return "".join(res)
         
