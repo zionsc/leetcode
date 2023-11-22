f5f8faa170982433e9b35a51a53a5ef94b280a8d
Seeon Chang <zionchan@usc.edu>
2023-11-21 20:20:53 -0800

    1424

--- Arrays & Hashing/1424. Diagonal Traverse II/1424.py
+++ Arrays & Hashing/1424. Diagonal Traverse II/1424.py
@@ -1,2 +1,8 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        res = defaultdict(deque)
+        for i, rows in enumerate(nums):
+            for j, val in enumerate(nums[i]):
+                res[i + j].appendleft(val)
+
+        return chain(*res.values())
