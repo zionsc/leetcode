8e4b1e2852a961ac896c0543ca3fceb948f2b554
Seeon Chang <zionchan@usc.edu>
2023-11-22 01:10:05 -0800

    0135

---
+++ Greedy/0135. Candy/0135.py
@@ -0,0 +1,14 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        res = [1] * len(ratings)
+        n = len(ratings)
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                res[i] = res[i - 1] + 1
+        
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                res[i] = max(res, res[i + 1] + 1)
+        
+        return sum(res)
