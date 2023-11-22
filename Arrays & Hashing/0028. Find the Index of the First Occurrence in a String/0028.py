fd4eeba96f1bf05b8137d5fafd5a83b74f0c7836
Seeon Chang <zionchan@usc.edu>
2023-11-22 02:13:26 -0800

    0028

--- Arrays & Hashing/0028. Find the Index of the First Occurrence in a String/0028.py
+++ Arrays & Hashing/0028. Find the Index of the First Occurrence in a String/0028.py
@@ -1,2 +1,10 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        
+        for l in range(len(haystack)):
+            # not possible to form needle anymore
+            if len(haystack) - l < len(needle):
+                return -1
+            
