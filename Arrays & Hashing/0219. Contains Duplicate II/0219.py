edc23adb674514636b1c00a17d7daf593299e874
Seeon Chang <zionchan@usc.edu>
2023-11-22 23:26:52 -0800

    0219

--- Arrays & Hashing/0219. Contains Duplicate II/0219.py
+++ Arrays & Hashing/0219. Contains Duplicate II/0219.py
@@ -3,9 +3,11 @@ class Solution:
         my_map = defaultdict(int)
 
         for i, n in enumerate(nums):
+            # basically if num has been seen before, then the next time we see it if the current map[n] is the closest
+            # last time we saw n, thus my_map[n] - i would be the smallest. we need it to be smaller then k so yes!!!
             if n in my_map and abs(my_map[n] - i) <= k:
                 return True
-            else:
+            else: # otherwise swap the my_map[n] to the current index of n --> i
                 my_map[n] = i
         
         return False
