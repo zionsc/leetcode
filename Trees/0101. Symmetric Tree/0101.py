b88c9b270350c667c2dd47df60f9a7161961efe8
Seeon Chang <zionchan@usc.edu>
2023-11-23 19:48:11 -0800

    0101

---
+++ Trees/0101. Symmetric Tree/0101.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        def dfs(left, right):
+            
+
+        return dfs(root.left, root.right)
