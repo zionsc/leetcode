e474b6097de54fbdc4becc9a9b688ae995d7d742
Seeon Chang <zionchan@usc.edu>
2023-11-26 02:04:14 -0800

    0061

---
+++ Linked List/0061. Rotate List/0061.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k == 0:
+            return head
+    
+        # make the linkedlist cyclic
+        last = head
+        length = 1
+        while last.next:
+            last = last.next
+            length += 1
+        last.next = head
+
+        k %= length
+        new_tail = head
+        for _ in range(length - k - 1):
+            new_tail = new_tail.next
+        
+        new_head = new_tail.next
+        new_tail.next = None
+        return new_head
