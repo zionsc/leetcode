bca8d91a7c1d0fa8ca4d93a2c8d5e26d642c06c3
Seeon Chang <zionchan@usc.edu>
2023-11-27 00:54:37 -0800

    0082

---
+++ Linked List/0082. Remove Duplicates from Sorted List II/0082.py
@@ -0,0 +1,10 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
