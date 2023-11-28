96a1abca2f4d91dd1367c733278c68f5762a8a48
Seeon Chang <zionchan@usc.edu>
2023-11-28 13:30:51 -0800

    0222

---
+++ Trees/0222. Count Complete Tree Nodes/0222.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.count = 0
+        def dfs(node):
+            if not node:
+                return
+            self.count += 1
+            dfs(node.left)
+            dfs(node.right) 
+
+        dfs(root)
+        return self.count   
