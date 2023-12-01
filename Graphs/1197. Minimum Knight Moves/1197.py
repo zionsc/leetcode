5fc26848a98cf65614ab1103acc591b4a713c300
Seeon Chang <zionchan@usc.edu>
2023-12-01 01:43:36 -0800

    1197

--- Graphs/1197. Minimum Knight Moves/1197.py
+++ Graphs/1197. Minimum Knight Moves/1197.py
@@ -1,3 +1,12 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        
+        res = 0
+
+        directions = [[2,1],[1,2],[-2,1],[1,-2],[-1,2],[2,-1],[-2,-1],[-1,-2]]
+
+        visited = set()
+        visited.add((0,0))
+        queue = deque()
+        queue.add((0,0))
+
+        while queue:
