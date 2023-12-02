0301cc4779f9b7da56c30dd37f06892dee1eab4f
Seeon Chang <zionchan@usc.edu>
2023-12-02 13:58:07 -0800

    0370

--- Math & Geometry/0370. Range Addition/0370.py
+++ Math & Geometry/0370. Range Addition/0370.py
@@ -1,2 +1,16 @@
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+        arr = [0 for _ in range(length)]
+
+        for start, end, inc in updates:
+            arr[start] += inc
+
+            if end + 1 < length:
+                # this basically cancels out the prefix sum after end. This is because we dont want to inc
+                # after the range start:end
+                arr[end + 1] -= inc
+            
+        for i in range(1, len(arr)):
+            arr[i] += arr[i - 1]
+
+        return arr
