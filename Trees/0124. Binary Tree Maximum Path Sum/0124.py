07e8ce92fe4313f478e608b56154202b5d7466aa
Seeon Chang <zionchan@usc.edu>
2023-12-01 21:45:06 -0800

    0124

--- Trees/0124. Binary Tree Maximum Path Sum/0124.py
+++ Trees/0124. Binary Tree Maximum Path Sum/0124.py
@@ -1,27 +1,17 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        self.res = float('-inf')
 
-        # Define a helper function to calculate the maximum path sum with a given node as the root
         def dfs(node):
-            nonlocal res  # Use the res variable from the outer function (maxPathSum)
             if not node:
                 return 0
+            
+            left = max(0, dfs(node.left))
+            right = max(0, dfs(node.right))
 
-            # Get the maximum sum for the left and right subtrees (ignore negative sums)
-            left_sum = max(dfs(node.left), 0)
-            right_sum = max(dfs(node.right), 0)
+            self.res = max(self.res, node.val, left + node.val + right)
 
-            # Uspdate the result with the maximum path sum that goes through the current node
-            res = max(res, node.val + left_sum + right_sum) # need to return max of res, or possible split in tree 
+            return max(node.val, node.val + left, node.val + right)
 
-            # Return the maximum sum that can be extended upwards (including the current node's value)
-            return node.val + max(left_sum, right_sum)
-
-            # need to update res -> result val to make sure it returns max possible, but need to return node.val + max(left, right) in order to tell next node up what its value currently is. 
-
-        res = float('-inf')  # Initialize the result to negative infinity
-        dfs(root)  # Start the recursive traversal
-
-        return res
+        dfs(root)
+        return self.res
