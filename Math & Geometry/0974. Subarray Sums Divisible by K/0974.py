10a8a3696adaec2fc6a6b1b36ec21bb755e10207
Seeon Chang <zionchan@usc.edu>
2023-12-02 12:05:12 -0800

    0974

---
+++ Math & Geometry/0974. Subarray Sums Divisible by K/0974.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remainder_count = defaultdict(int)
+        remainder_count[0] = 1
+
+        total = 0
+        res = 0
+
+        for i in range(len(nums)):
+            total += nums[i]
+            remainder = total % k
+            if remainder < 0:
+                remainder += k
+            
+
