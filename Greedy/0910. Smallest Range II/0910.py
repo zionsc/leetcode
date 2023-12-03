4c016fecf167221c1c6a017f38348b650c6c0933
Seeon Chang <zionchan@usc.edu>
2023-12-02 23:28:33 -0800

    0910

--- Greedy/0910. Smallest Range II/0910.py
+++ Greedy/0910. Smallest Range II/0910.py
@@ -1,2 +1,11 @@
 class Solution:
     def smallestRangeII(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        small = nums[0]
+        big = nums[-1]
+        score = big - small
+        
+        for i in range(len(nums) - 1):
+            score = min(score, max(big - k, nums[i] + k) - min(small + k, nums[i + 1] - k))
+        
+        return score
