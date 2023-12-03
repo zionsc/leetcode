e117c3d2c4c92ddf9c466366258b3c01fd7c366d
Seeon Chang <zionchan@usc.edu>
2023-12-03 15:36:46 -0800

    1010

--- Math & Geometry/1010. Pairs of Songs With Total Durations Divisible by 60/1010.py
+++ Math & Geometry/1010. Pairs of Songs With Total Durations Divisible by 60/1010.py
@@ -1,2 +1,15 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        res = 0
+        freq_map = Counter(t % 60 for t in time)
+
+        for i,v in freq_map.items():
+            if 60 - i in freq_map and v > 0 and freq_map[60 - i] > 0 or i == 0:
+                if i == 30 or i == 0:
+                    res += comb(v, 2)
+                    freq_map[i] = 0
+                else:
+                    res += v * freq_map[60 - i]
+                    freq_map[i] = 0
+                    freq_map[60 - i] = 0
+        return res
