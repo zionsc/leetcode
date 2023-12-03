c91866657ce0a2fe057250ddb12a2a05198879ad
Seeon Chang <zionchan@usc.edu>
2023-12-02 16:16:46 -0800

    1266

--- Math & Geometry/1266. Minimum Time Visiting All Points/1266.py
+++ Math & Geometry/1266. Minimum Time Visiting All Points/1266.py
@@ -1,10 +1,13 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
         res = 0
-
+        
         for i in range(len(points) - 1):
             curr_x, curr_y = points[i]
             target_x, target_y = points[i + 1]
+            
+            # essentially because the maximum of the distances between the x and y coordinates determine the time
+            # since we can also go diagonally.
             res += max(abs(target_x - curr_x), abs(target_y - curr_y))
 
         return res
