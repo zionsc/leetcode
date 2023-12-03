0f7689013c7474d6245e18c70574a09e09737e29
Seeon Chang <zionchan@usc.edu>
2023-12-02 23:29:23 -0800

    0910

--- Greedy/0910. Smallest Range II/0910.py
+++ Greedy/0910. Smallest Range II/0910.py
@@ -6,6 +6,8 @@ class Solution:
         score = big - small
         
         for i in range(len(nums) - 1):
+            # testing all ranges possibly simply nums[i] + k could be slightly bigger than big-k!
+            # nums[i + 1] - k might be smaller smaller than small + k
             score = min(score, max(big - k, nums[i] + k) - min(small + k, nums[i + 1] - k))
         
         return score
