9a89a10ef98a8f34a3ee035a5b03383233458a10
Seeon Chang <zionchan@usc.edu>
2023-12-04 11:34:22 -0800

    0259

--- Two Pointers/0259. 3Sum Smaller/0259.py
+++ Two Pointers/0259. 3Sum Smaller/0259.py
@@ -1,2 +1,15 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        res = 0
+
+        for i,a in enumerate(nums):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threesome = a + nums[l] + nums[r]
+                if threesome >= target:
+                    r -= 1
+                else:
+                    res += r - l
+                    l += 1
+        return res
