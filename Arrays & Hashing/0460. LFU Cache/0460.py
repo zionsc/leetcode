3c657251e9b080a158f617d4a071fa07fa31c70a
Seeon Chang <zionchan@usc.edu>
2023-12-05 01:28:02 -0800

    0460

--- Arrays & Hashing/0460. LFU Cache/0460.py
+++ Arrays & Hashing/0460. LFU Cache/0460.py
@@ -22,7 +22,24 @@ class LFUCache:
         
         return self.frequencies[f + 1][key]
                         
-
-
-
     def put(self, key: int, value: int) -> None:
+        
+        if len(self.values) == self.capacity:
+            if not self.values[key]:
+                k = self.frequencies[self.min_freq].popitem(last=False) # FIFO --> first in first out (queue) (LRU)
+                del self.values[k[0]]
+
+        f = self.values[key]
+        self.values[key] += 1
+
+        if f != 0:
+            self.frequencies[f + 1][key] = value
+            del self.frequencies[f][key]
+            
+            if self.min_freq == f and len(self.frequencies[f]) == 0:
+                self.min_freq = f + 1
+        else:
+            self.frequencies[f + 1][key] = value
+
+        self.min_freq = min(self.values[key], self.min_freq)
+
