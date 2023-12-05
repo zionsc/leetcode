410840c222b01914ef4faf6d74e1dc74f4c15d1a
Seeon Chang <zionchan@usc.edu>
2023-12-05 01:12:17 -0800

    0460

--- Arrays & Hashing/0460. LFU Cache/0460.py
+++ Arrays & Hashing/0460. LFU Cache/0460.py
@@ -6,7 +6,23 @@ class LFUCache:
         self.frequencies = defaultdict(lambda: OrderedDict())
 
     def get(self, key: int) -> int:
+        f = self.values[key] # get the frequency
         
+        if not f:
+            del self.values[key]
+            return -1
+        
+        self.frequencies[f + 1][key] = self.frequencies[f][key]
+        self.values[key] = f + 1
+
+        del self.frequencies[f][key]
+
+        if self.min_freq == f and len(self.frequencies[f]) == 0:
+            self.min_freq = f + 1
+        
+        return self.frequencies[f + 1][key]
+                        
+
 
 
     def put(self, key: int, value: int) -> None:
