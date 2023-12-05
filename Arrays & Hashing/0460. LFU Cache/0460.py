55af4151f8d5609b5bcf73b45ff9f914fe5c2f11
Seeon Chang <zionchan@usc.edu>
2023-12-05 00:46:07 -0800

    0460

--- Arrays & Hashing/0460. LFU Cache/0460.py
+++ Arrays & Hashing/0460. LFU Cache/0460.py
@@ -1,2 +1,12 @@
 class LFUCache:
     def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.values = defaultdict(int)
+        self.min_freq = float('inf')
+        self.frequencies = defaultdict(lambda: OrderedDict())
+
+    def get(self, key: int) -> int:
+        
+
+
+    def put(self, key: int, value: int) -> None:
