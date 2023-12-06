644762898c5a3347abd6ebc926c0101385c88a72
Seeon Chang <zionchan@usc.edu>
2023-12-05 16:43:52 -0800

    1716

--- Math & Geometry/1716. Calculate Money in Leetcode Bank/1716.py
+++ Math & Geometry/1716. Calculate Money in Leetcode Bank/1716.py
@@ -1,2 +1,15 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
+        res = 0
+        weeks = n // 7
+        days_last_week = n % 7
+        week = 0
+
+        for i in range(1, weeks + 1):
+            res += (7 * (7 + 1)) // 2
+            res += 7 * weeks
+            weeks += 1
+        
+        res += (days_last_week * (days_last_week + 1)) // 2
+        res += week * days_last_week
+        return res
