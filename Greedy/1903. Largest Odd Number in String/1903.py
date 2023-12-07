38d8c31850630498322110dc6c9b5a8c06dc8b9e
Seeon Chang <zionchan@usc.edu>
2023-12-06 16:40:36 -0800

    1903

---
+++ Greedy/1903. Largest Odd Number in String/1903.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestOddNumber(self, nums: str) -> str:
+      for i in range(len(nums) - 1, -1, -1):
+        if int(nums[i]) % 2 == 1:
+          return nums[:i + 1]
+      return ""
