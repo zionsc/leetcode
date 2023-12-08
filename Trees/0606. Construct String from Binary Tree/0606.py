02226185aba0e6faa407b2e9ff0b30f1059f4941
Seeon Chang <zionchan@usc.edu>
2023-12-07 19:41:04 -0800

    0606

---
+++ Trees/0606. Construct String from Binary Tree/0606.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+      if not root:
+        return ""
+
+      res = str(root.val)
+      
+      if root.left or root.right:
+        res += f"({self.tree2str(root.left)})"
+      if root.right:
+        res += f"({self.tree2str(root.right)})"
+      return res
+
+      
+          
