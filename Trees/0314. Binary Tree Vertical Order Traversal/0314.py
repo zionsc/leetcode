3b1ffa59bcf0df524d35fc671cc0a2d9186969e6
Seeon Chang <zionchan@usc.edu>
2023-12-09 15:51:30 -0800

    0314

--- Trees/0314. Binary Tree Vertical Order Traversal/0314.py
+++ Trees/0314. Binary Tree Vertical Order Traversal/0314.py
@@ -6,3 +6,16 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = defaultdict(list)
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node:
+                res[column].append(node.val)
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+        
+        keys = sorted(res.keys())
+        return [res[key] for key in keys]
