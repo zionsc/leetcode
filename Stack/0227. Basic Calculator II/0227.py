e1af7205c568234f8983e8558332c8cfa1333139
Seeon Chang <zionchan@usc.edu>
2023-12-12 19:53:43 -0800

    0227

---
+++ Stack/0227. Basic Calculator II/0227.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        op = '+'
+
+        # the + at the end to parse the last operation --> basically this funct does the last operation found
+        for c in s + '+':
+            if c.isdigit():
+                num = (num * 10) + int(c)
+            elif c == ' ':
+                continue
+            else: # c is an operation
+                if c == '+':
+                    stack.append(num)
+                elif c == '-':
+                    stack.append(-num)
+                elif c == '*':
+                    stack.append(stack.pop() * num)
+                elif c == '/':
+                    stack.append(int(stack.pop() / num))
+
+                num = 0
+                op = c
+
+        return sum(stack)
