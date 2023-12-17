8f671f52fb64bbaa91cbb2c1566a13fa45405e3c
Seeon Chang <zionchan@usc.edu>
2023-12-16 22:14:58 -0800

    2353

--- Heap Priority Queue/2353. Design a Food Rating System/2353.py
+++ Heap Priority Queue/2353. Design a Food Rating System/2353.py
@@ -1,7 +1,9 @@
 class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        # need ratings to see if the food is actually updated
         self.ratings = defaultdict(int)
+        # need cuisine[food] = cuisine as we need o(1) access to what cuisine the food is
         self.cuisine = {}
         self.by_cuisine = defaultdict(list)
 
@@ -11,6 +13,8 @@ class FoodRatings:
             heapq.heappush(self.by_cuisine[cuisine], (-rating, food))
 
     def changeRating(self, food: str, newRating: int) -> None:
-        
+        self.ratings[food] = newRating
+        cuisine = self.cuisine[food]
+        heapq.heappush(self.by_cuisine[cuisine], (-newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
