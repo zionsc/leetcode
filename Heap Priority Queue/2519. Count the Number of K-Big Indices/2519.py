3e13b172811c1183cc67ddc8555ab4a1055b60c0
Seeon Chang <zionchan@usc.edu>
2023-12-19 05:26:44 -0800

    2519

--- Heap Priority Queue/2519. Count the Number of K-Big Indices/2519.py
+++ Heap Priority Queue/2519. Count the Number of K-Big Indices/2519.py
@@ -1,8 +1,10 @@
 class Solution:
     def kBigIndices(self, nums: List[int], k: int) -> int:
         # always less than, but k indices that are ahead of i and k indices before 
-        n = len(nums)
         res = 0
         left = [-nums[i] for i in range(k)]
-        right = [-nums[i] for i in range(n - 1, n - k - 1, -1)]
-        
+        right = [-nums[i] for i in range(len(nums) - 1, len(nums) - k - 1, -1)]
+
+        left_condition = [0 for _ in range(len(nums))]
+        for i in range(k, len(nums) - k):
+            
