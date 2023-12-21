7822092547c3de41ca755ceaa1c6097b0d201904
Seeon Chang <zionchan@usc.edu>
2023-12-21 00:18:17 -0800

    2458

--- Trees/2458. Height of Binary Tree After Subtree Removal Queries/2458.py
+++ Trees/2458. Height of Binary Tree After Subtree Removal Queries/2458.py
@@ -14,16 +14,25 @@ class Solution:
                 return -1
             curr = 1 + max(dfs(node.left, depth + 1), dfs(node.right, depth + 1))
             depth_map[node.val] = depth
-            height[node.val] = curr
+            height_map[node.val] = curr
             return curr
 
         dfs(root, 0)
 
         depth_to_node = defaultdict(list)
         for node, depth in depth_map.items():
-            depth_to_node[depth].append(height[node], node)
+            depth_to_node[depth].append((height_map[node], node))
             depth_to_node[depth].sort(reverse=True)
             if len(depth_to_node[depth]) > 2:
                 depth_to_node[depth].pop()
 
-        
+        res = []
+        for query in queries:
+            depth = depth_map[query]
+            if len(depth_to_node[depth]) == 1:
+                res.append(depth - 1)
+            elif depth_to_node[depth][0][1] == query:
+                res.append(depth_to_node[depth][1][0] + depth)
+            else:
+                res.append(depth_to_node[depth][0][0] + depth)
+        return res
