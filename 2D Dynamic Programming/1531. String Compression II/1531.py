be8a11633eedc292cba0618ab5b1e95b4f5a38ab
Seeon Chang <zionchan@usc.edu>
2023-12-28 05:52:51 -0800

    1531

--- 2D Dynamic Programming/1531. String Compression II/1531.py
+++ 2D Dynamic Programming/1531. String Compression II/1531.py
@@ -1,2 +1,12 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        
+        def dfs(i, prev, prev_count, k):
+
+            if s[i] == prev:
+
+            else:
+                # essentially if they are not the same, I want to remove or keep
+                res = min(
+
+                )
