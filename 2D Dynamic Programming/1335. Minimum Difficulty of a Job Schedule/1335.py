8a2b59b28c6903a9149c08630729a5c90f1c15e0
Seeon Chang <zionchan@usc.edu>
2023-12-28 19:30:40 -0800

    1335

--- 2D Dynamic Programming/1335. Minimum Difficulty of a Job Schedule/1335.py
+++ 2D Dynamic Programming/1335. Minimum Difficulty of a Job Schedule/1335.py
@@ -1,2 +1,12 @@
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        if d > len(jobDifficulty):
+            return -1
+        
+        memo = {}
+
+        # keep track of curr_max in order to add to result if we start a new day
+        def dfs(i, d, curr_max):
+            if i == len(jobDifficulty):
+                return 0 if d == 0 else float('inf')
+            
