c6c8e25ec9b7238e26f7aff2322faaa19d10112d
Seeon Chang <zionchan@usc.edu>
2023-12-31 16:19:45 -0800

    0455

---
+++ Greedy/0455. Assign Cookies/0455.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort(),s.sort()
+
+        res = 0
+        i,j = 0,0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                res += 1
+                i += 1
+            j += 1
+        return res
