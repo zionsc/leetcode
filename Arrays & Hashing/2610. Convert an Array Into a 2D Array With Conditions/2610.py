4627c64c538ed98aaef0ad1ba90140e25dc2eaa1
Seeon Chang <zionchan@usc.edu>
2024-01-02 04:39:41 -0800

    2610

--- Arrays & Hashing/2610. Convert an Array Into a 2D Array With Conditions/2610.py
+++ Arrays & Hashing/2610. Convert an Array Into a 2D Array With Conditions/2610.py
@@ -1,17 +1,11 @@
-    class Solution:
-        def findMatrix(self, nums: List[int]) -> List[List[int]]:
-            res = []
-            for num in nums:
-                found = False
-                for group in res:
-                    if num in group:
-                        continue
-                    else:
-                        found = True
-                        group.add(num)
-                        break
-                if not found:
-                    res.append(set([num]))
-            return res
-                
-        
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        count = defaultdict(int)
+        for num in nums:
+            row = count[num]
+            if row == len(res):
+                res.append([])
+            res[row].append(num)
+            count[num] += 1
+        return res
