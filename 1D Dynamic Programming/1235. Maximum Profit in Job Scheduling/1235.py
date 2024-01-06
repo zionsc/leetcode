be8e2bd58b6dba566bd026bc246fc277a61af6f4
Seeon Chang <zionchan@usc.edu>
2024-01-05 18:59:56 -0800

    1235

--- 1D Dynamic Programming/1235. Maximum Profit in Job Scheduling/1235.py
+++ 1D Dynamic Programming/1235. Maximum Profit in Job Scheduling/1235.py
@@ -1,3 +1,10 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        # we want to take this start time, or take optimal solution before it 
+        n = len(startTime)
+        dp = [0] * (n + 1) # for the first iteration we need to go out of bounds 
+
+        # keep track of the start time and the index in order to match with the endtime and profit later
+        start = [(startTime[i], i) for i in range(n)]
+
         
