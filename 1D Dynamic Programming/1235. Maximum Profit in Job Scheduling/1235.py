835142cc7490f809f8993cd5e4e5c789f2d70f10
Seeon Chang <zionchan@usc.edu>
2024-01-05 19:10:35 -0800

    1235

--- 1D Dynamic Programming/1235. Maximum Profit in Job Scheduling/1235.py
+++ 1D Dynamic Programming/1235. Maximum Profit in Job Scheduling/1235.py
@@ -6,7 +6,12 @@ class Solution:
 
         # keep track of the start time and the index in order to match with the endtime and profit later
         start = [(startTime[i], i) for i in range(n)]
+        start.sort()
 
-
-        # we take the job, or we take the optimal solution that was considered before our job (dp[i + 1])
-        dp[i] = max(dp[i + 1], profit[start[i][1]] + dp[next_compatible])
+        for i in range(n - 1, -1, -1):
+            # look for the next optimal one that starts after current one ends (binary search)
+            # the 0 next to endTime is just placeholder since we have to search for a tuple in start array
+            next_compatible = bisect.bisect_left(start, (endTime[start[i][1]], 0), i + 1)
+            # we take the job, or we take the optimal solution that was considered before our job (dp[i + 1])
+            dp[i] = max(dp[i + 1], profit[start[i][1]] + dp[next_compatible])
+        return dp[0]
