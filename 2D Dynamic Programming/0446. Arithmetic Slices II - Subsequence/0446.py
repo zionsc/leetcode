2b5d0d143f3a69259c699a8e7dafb535d57bfc68
Seeon Chang <zionchan@usc.edu>
2024-01-06 18:08:03 -0800

    0446

--- 2D Dynamic Programming/0446. Arithmetic Slices II - Subsequence/0446.py
+++ 2D Dynamic Programming/0446. Arithmetic Slices II - Subsequence/0446.py
@@ -1,3 +1,11 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        
+        res = 0
+        dp = [defaultdict(int) for _ in range(len(nums))]
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += 1 + dp[j][diff]
+                res += dp[j][diff]
+        return res
