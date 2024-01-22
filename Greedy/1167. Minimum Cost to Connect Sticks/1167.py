03441ba244774eb8f34365161a5df7180b742838
Seeon Chang <zionchan@usc.edu>
2024-01-22 12:26:21 -0800

    1167

---
+++ Greedy/1167. Minimum Cost to Connect Sticks/1167.py
@@ -0,0 +1,10 @@
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        res = 0
+        heapq.heapify(sticks)
+        while len(sticks) > 1:
+            x = heapq.heappop(sticks)
+            y = heapq.heappop(sticks)
+            heapq.heappush(sticks, x + y)
+            res += x + y
+        return res
