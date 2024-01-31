4a1e6ec62ac82bafb439ee532709d5f2d7ec305f
Seeon Chang <zionchan@usc.edu>
2024-01-30 16:59:08 -0800

    0739

--- Stack/0739. Daily Temperatures/0739.py
+++ Stack/0739. Daily Temperatures/0739.py
@@ -1,14 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = [] # pair (temp, index)
+        stack = []
 
-        for i, t in enumerate(temperatures): # enumerate must be used in order to get both index and value
-            # this works as the values that are skipped over will be checked by stack as they are popped
-            while stack and t > stack[-1][0]:
-                temp, index = stack.pop()
-                res[index] = i - index
-            stack.append((t, i)) # if the current temp is less than the last temp, add it to the stack
+        for i,temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stack_index, stack_temp = stack.pop()
+                res[stack_index] = i - stack_index
+            stack.append((temp, i))
 
         return res
-    
