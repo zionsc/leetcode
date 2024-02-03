8b52c446f1530a408fcc80efe0e232f351fb53c9
Seeon Chang <zionchan@usc.edu>
2024-02-02 18:09:48 -0800

    1043

--- 2D Dynamic Programming/1043. Partition Array for Maximum Sum/1043.py
+++ 2D Dynamic Programming/1043. Partition Array for Maximum Sum/1043.py
@@ -1,2 +1,15 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [0] * (n + 1)
+
+        for i in range(n):
+            curr_sum = curr_max = 0
+            for j in range(i, max(-1, i - k), -1):
+                if curr_max < arr[j]:
+                    curr_max = arr[j]
+                curr = curr_max * (i - j + 1) + dp[j]
+                if curr_sum < curr:
+                    curr_sum = curr
+            dp[i + 1] = curr_sum
+        return dp[-1]
