a6a1c5d5bfe6e213a46f95741b3ed07834e5b8c4
Seeon Chang <zionchan@usc.edu>
2024-02-22 18:07:19 -0800

    0787

--- Graphs/0787. Cheapest Flights Within K Stops/0787.py
+++ Graphs/0787. Cheapest Flights Within K Stops/0787.py
@@ -1,2 +1,21 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # creating the adjacency list
+        adj_list = defaultdict(list)
+        for f,t,p in flights:
+            adj_list[f].append([t,p])
+
+        # kept as res but also to stop if iteration ever larger than before cache
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        queue = deque([(0,src,0)]) # price, node, steps
+
+        while queue:
+            curr_price, curr_node, curr_steps = queue.popleft()
+            for nei_node,nei_price in adj_list[curr_node]:
+                if curr_price + nei_price < prices[nei_node] and curr_steps <= k:
+                    prices[nei_node] = curr_price + nei_price
+                    queue.append((curr_price + nei_price, nei_node, curr_steps + 1))
+        
+        return prices[dst] if prices[dst] != float('inf') else -1
