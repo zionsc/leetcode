c7e7fcd2653acb8e4c9a8ade7b8d2dd310c2b616
Seeon Chang <zionchan@usc.edu>
2024-02-27 16:14:34 -0800

    0513

--- Trees/0513. Find Bottom Left Tree Value/0513.py
+++ Trees/0513. Find Bottom Left Tree Value/0513.py
@@ -19,3 +19,25 @@ class Solution:
         
         dfs(root, 0)
         return self.res[0]
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+#         res = [0,float('inf')]
+#         queue = deque([(root, 0)])
+#         while queue:
+#             node,level = queue.popleft()
+#             if not node.left and not node.right and level < res[1]:
+#                 res = [node.val, level]
+#             if node.left:
+#                 queue.append((node.left, level - 1))
+#             if node.right:
+#                 queue.append((node.right, level - 1))
+#         return res[0]
+        
