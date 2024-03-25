fc6cfa1da00031eb226ede2fc787b08cbbb15099
Seeon Chang <zionchan@usc.edu>
2024-03-24 22:09:53 -0700

    0442

--- Arrays & Hashing/0442. Find All Duplicates in an Array/0442.cpp
+++ Arrays & Hashing/0442. Find All Duplicates in an Array/0442.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+
         vector<int> res;
         for (int& num : nums) {
-            int idx = abs(num) - 1;
+            int idx = abs(nums) - 1;
             if (nums[idx] < 0) {
                 res.push_back(abs(num));
             }
