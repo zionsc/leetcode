7e1d17f84f933bc9314598b9725a72ea060eb739
Seeon Chang <zionchan@usc.edu>
2024-03-29 19:01:12 -0700

    0992

--- Sliding Window/0992. Subarrays with K Different Integers/0992.cpp
+++ Sliding Window/0992. Subarrays with K Different Integers/0992.cpp
@@ -2,25 +2,25 @@
 
 class Solution {
 public:
-    int atMostK(vector<int> arr, int k) {
-        vector<int> freq(arr.size()+1);
+    int atMostK(vector<int>& nums, int k) {
+        vector<int> freq(nums.size()+1,0);
         int res=0;
         int l=0;
 
-        for(int r=0; r<arr.size(); ++r) {
-            freq[arr[r]]++;
-            if(freq[arr[r]]==1){
+        for(int r=0; r<nums.size(); ++r){
+            freq[nums[r]]++;
+            if(freq[nums[r]]==1){
                 k--;
             }
             while(k<0){
-                freq[arr[l]]--;
-                if(freq[arr[l]]==0){
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0){
                     k++;
                 }
                 l++;
             }
             res+=(r-l+1);
-        }
+        } 
         return res;
     }
 
@@ -28,8 +28,7 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        
-        return (atMostK(nums,k) - atMostK(nums,k-1));
+
+        return atMostK(nums,k)-atMostK(nums,k-1);
     }
-    
 };
