36a2aba6f6694c73d8d92f539c68729bd6f5e136
Seeon Chang <zionchan@usc.edu>
2024-03-30 20:49:49 -0700

    2444

---
+++ Sliding Window/2444. Count Subarrays With Fixed Bounds/2444.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("03", "unroll-loops")
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        long long res=0;
+        int l=0;
+        int lastMin,lastMax=-1;
+
+        for(int r=0; r<nums.size(); ++r){
+            if(nums[r]<minK || nums[r]>maxK){
+                l=r+1;
+                lastMin,lastMax=-1;
+            }
+            if(nums[r]==minK){
+                lastMin=r;
+            }
+            if(nums[r]==maxK){
+                lastMax=r;
+            }
+            if(lastMin!=-1 && lastMax!=-1){
+                res+=min(lastMin,lastMax)-l+1;
+            }
+        }
+        return res;
+    }
+};
