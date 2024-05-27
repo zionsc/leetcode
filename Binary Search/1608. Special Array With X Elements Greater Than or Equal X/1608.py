e89d03ff849f1c8ef3508b21e93399dd704ec6f5
Seeon Chang <zionchan@usc.edu>
2024-05-26 21:52:44 -0700

    1608

--- Binary Search/1608. Special Array With X Elements Greater Than or Equal X/1608.py
+++ Binary Search/1608. Special Array With X Elements Greater Than or Equal X/1608.py
@@ -4,28 +4,28 @@ class Solution:
         n=len(nums)
         l,r=0,n
 
-        while (l<=r):
-            mid=(l+r)//2
-            pos=bisect_left(nums,mid)
-            pos=binSearch(mid)
-            count=n-pos
+        def bisectleft(target):
+            l,r=0,n
 
-            if (count==mid):
+            while l < r:
+                mid = (l + r) // 2
+                if nums[mid] >= target:
+                    r = mid
+                elif nums[mid] < target:
+                    l = mid + 1
+            return l
+        
+        while l <= r:
+            mid = (l + r) // 2
+            pos = bisect_left(nums, mid)
+            # pos = bisectleft(mid)
+            count = n - pos
+
+            if count == mid:
                 return mid
-            elif (count>mid):
-                l=mid+1
-            elif (count<mid):
-                r=mid-1
-
-        # def binSearch(target):
-        #     l,r=0,n
-        #     while l<=r:
-        #         mid=(l+r)//2
-        #         if (nums[mid]>=target):
-        #             r=mid
-        #         elif (nums[mid]<target):
-        #             l=mid+1
-        #     return l
-
-
+            elif count > mid:
+                l = mid + 1
+            elif count < mid:
+                r = mid - 1
+        
         return -1
