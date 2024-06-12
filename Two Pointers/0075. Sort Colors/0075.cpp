e8a8cba03a29fb7ac023b7d0c94a72a2897390ff
Seeon Chang <zionchan@usc.edu>
2024-06-12 00:19:02 -0700

    0075

---
+++ Two Pointers/0075. Sort Colors/0075.cpp
@@ -0,0 +1,12 @@
+#pragma GCC optimize("O3","Oz","unroll-loops","inline-functions","fast-math","march=native","lto","omit-frame-pointer")
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        sort(nums.begin(), nums.end());
+
+    }
+};
